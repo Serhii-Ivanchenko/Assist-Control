92bc4c9a8b24823db464bc824d4cdf2dc4d24818
Juliiaks <juliakharetonchuk@gmail.com>
2024-10-01 10:39:31 +0300

    changes

--- src/components/UserSettingsModal/UserSettingsModal.jsx
+++ src/components/UserSettingsModal/UserSettingsModal.jsx
@@ -1,15 +1,45 @@
 import { Field, Form, Formik } from "formik";
+import { useId } from "react";
+import * as Yup from "yup";
+import css from './UserSettingsModal.module.css'
+
+const Validation = Yup.object().shape({
+    username: Yup.string().min(2, "Занадто коротке").max(30, "Занадто довге").required("Обов'язкове поле для заповнення"),
+    email: Yup.string().email().required("Обов'язкове поле для заповнення"),
+    phone: Yup.string().min(3, "Занадто коротке").max(50, "Занадто довге").required("Обов'язкове поле для заповнення"),
+    company: Yup.string().min(2).max(50).required("Обов'язкове поле для заповнення")
+});
+    
+const initialValues = {
+    username: '',
+    email: '',
+    phone: '',
+    company:''
+}
 
 export default function UserSettingsModal() {
+
+   
+
+    const nameFieldId = useId();
+    const emailFieldId = useId();
+    const phoneFieldId = useId();
+    const companyFieldId = useId();
+
     return (
-        <Formik initialValues={{}} onSubmit={() => {}}>
-            <Form>
+        <Formik initialValues={initialValues} onSubmit={() => {}} validationSchema={Validation}>
+            <Form className={css.formBox}>
                 <Field type='file' name='photo' />
-                <Field type='text' name='username' />
-                <Field type='email' name='email' />
-                <Field type='' name='phone' />
-                <Field type='text' name='company' />
+                <label htmlFor={nameFieldId}>Ім&apos;я</label>
+                <Field type='text' name='username' id={nameFieldId} />
+                <label htmlFor={emailFieldId}>Пошта</label>
+                <Field type='email' name='email' id={emailFieldId} />
+                <label htmlFor={phoneFieldId}>Номер телефону</label>
+                <Field type='' name='phone' id={phoneFieldId} />
+                <label htmlFor={companyFieldId}>Назва компанії</label>
+                <Field type='text' name='company' id={companyFieldId} />
                 <button type="submit">Зберегти</button>
+                <button type="button">Відміна</button>
             </Form>
         </Formik>
     )
