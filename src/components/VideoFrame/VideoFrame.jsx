c4411c1e690f2dbca21f680f9c40fe22e2d2ebed
Volodymyr <palcinskijvolodimir173@gmail.com>
2024-10-04 16:46:40 +0300

    add open in large window and zooming photo

--- src/components/VideoFrame/VideoFrame.jsx
+++ src/components/VideoFrame/VideoFrame.jsx
@@ -1,16 +1,22 @@
 import Slider from "react-slick";
 import cameraView from "../../assets/images/cameraView.png";
 import car from "../../assets/images/car.png";
+import road from "../../assets/images/pexels-photo-1563355.jpeg";
 import css from "./VideoFrame.module.css";
 import { IoIosArrowBack } from "react-icons/io";
 import { IoIosArrowForward } from "react-icons/io";
 import { VscZoomIn } from "react-icons/vsc";
+import { VscZoomOut } from "react-icons/vsc";
 import { BsCameraVideoOffFill } from "react-icons/bs";
+import Zoom from "react-medium-image-zoom";
+import "react-medium-image-zoom/dist/styles.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import InnerImageZoom from "react-inner-image-zoom";
+import "react-inner-image-zoom/lib/InnerImageZoom/styles.css";
+import { useEffect, useState } from "react";
 
 function SampleNextArrow(props) {
-  console.log(props);
   const { currentSlide, slideCount, onClick } = props;
   if (currentSlide === slideCount - 1) return null;
   return <IoIosArrowForward className={css.arrowNext} onClick={onClick} />;
@@ -22,6 +28,11 @@ function SamplePrevArrow(props) {
   return <IoIosArrowBack className={css.arrowPrev} onClick={onClick} />;
 }
 export default function VideoFrame() {
+  const [isZoomed, setIsZoomed] = useState(false);
+  const handleOpenModal = () => setIsZoomed(true);
+  const changeZoom = () => setIsZoomed(!isZoomed);
+  console.log(isZoomed);
+
   const settings = {
     dots: true,
     dotsClass: "dots",
@@ -34,15 +45,68 @@ export default function VideoFrame() {
     nextArrow: <SampleNextArrow />,
     prevArrow: <SamplePrevArrow />,
   };
+  function CameraModal({ changeZoom, modalState }) {
+    useEffect(() => {
+      const handleEscape = (event) => {
+        if (event.key === "Escape") {
+          changeZoom();
+        }
+      };
+      document.addEventListener("keydown", handleEscape);
+      return () => {
+        document.removeEventListener("keydown", handleEscape);
+      };
+    }, [changeZoom]);
+
+    if (modalState === "UNLOADING") {
+      changeZoom();
+    }
+    return (
+      <div className={css.zoomImgCont}>
+        <div className={css.zoomImg}>
+          <div className={css.onZoomIcon}>
+            <VscZoomOut
+              cursor={"pointer"}
+              size={40}
+              onClick={() => setIsZoomed(false)}
+            />
+          </div>
+          <InnerImageZoom src={road} zoomType="click" zoomScale={3} />
+        </div>
+      </div>
+    );
+  }
   return (
     <div className={css.cameraListCont}>
       <div className={css.cameraList}>
         <div className={css.zoomIcon}>
-          <VscZoomIn size={20} />
+          <VscZoomIn
+            cursor={"pointer"}
+            size={20}
+            onClick={() => handleOpenModal()}
+          />
         </div>
         <Slider {...settings}>
           <div className={css.camera}>
-            <img src={cameraView} alt="" />
+            {isZoomed ? (
+              <Zoom
+                isZoomed={isZoomed}
+                onZoomChange={changeZoom}
+                zoomImg={<InnerImageZoom src={road} zoomType="click" />}
+                ZoomContent={({ img, buttonUnzoom, modalState }) => (
+                  <CameraModal
+                    changeZoom={changeZoom}
+                    img={img}
+                    buttonUnzoom={buttonUnzoom}
+                    modalState={modalState}
+                  />
+                )}
+              >
+                <img src={road} alt="" width={{ width: 100 }} />
+              </Zoom>
+            ) : (
+              <img src={road} alt="" width={{ width: 100 }} />
+            )}
           </div>
           <div className={css.camera}>
             <img src={cameraView} alt="" />
