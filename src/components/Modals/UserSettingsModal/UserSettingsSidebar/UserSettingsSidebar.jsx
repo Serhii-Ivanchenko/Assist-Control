da74a909180af89e6ce235f4abc728a5e6f845a3
Juliiaks <juliakharetonchuk@gmail.com>
2024-10-05 00:44:26 +0300

    added icons

--- src/components/Modals/UserSettingsModal/UserSettingsSidebar/UserSettingsSidebar.jsx
+++ src/components/Modals/UserSettingsModal/UserSettingsSidebar/UserSettingsSidebar.jsx
@@ -1,6 +1,14 @@
 import { NavLink } from "react-router-dom";
 import css from "./UserSettingsSidebar.module.css"
 import clsx from "clsx";
+import { HiOutlineUser } from "react-icons/hi2";
+import { HiOutlineUsers } from "react-icons/hi2";
+import { CiSettings } from "react-icons/ci";
+import { FiCreditCard } from "react-icons/fi";
+
+
+
+
 
 export default function UserSettingsSidebar() {
     return (
@@ -15,24 +23,28 @@ export default function UserSettingsSidebar() {
                     <li className={css.sideBarNavItem}>
                         <NavLink to="" className={({ isActive }) =>
                                 clsx(css.sideBarNavItemLink, { [css.linkIsActive]: isActive })}>
+                                    <HiOutlineUser width={13} height={15}/>
                             <p>Профіль</p>
                         </NavLink>
                     </li>
                     <li className={css.sideBarNavItem}>
                         <NavLink to="*" className={({ isActive }) =>
                                 clsx(css.sideBarNavItemLink, { [css.linkIsActive]: isActive })}>
+                                    <CiSettings/>
                             <p>Акаунт</p>
                         </NavLink>
                     </li>
                     <li className={css.sideBarNavItem}>
                          <NavLink to="**" className={({ isActive }) =>
                                 clsx(css.sideBarNavItemLink, { [css.linkIsActive]: isActive })}>
+                                    <FiCreditCard width={13} height={8}/>
                             <p>Тариф</p>
                         </NavLink>
                     </li>
                     <li className={css.sideBarNavItem}>
                          <NavLink to="***" className={({ isActive }) =>
                                 clsx(css.sideBarNavItemLink, { [css.linkIsActive]: isActive })}>
+                                    <HiOutlineUsers/>
                             <p>Команда</p>
                         </NavLink>
                     </li>
