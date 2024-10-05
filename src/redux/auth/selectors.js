3f1cb315b32e0f7bf7bb830c0a7ceaf0a27162df
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-10-05 19:42:57 +0300

    add store + operations, slice, selectors for auth
    add all operations for auth except register, login, emailvalidation and resetpassword

---
+++ src/redux/auth/selectors.js
@@ -0,0 +1,14 @@
+export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
+
+export const selectIsRefreshing = (state) => state.auth.isRefreshing;
+
+export const selectLoading = (state) => state.auth.isLoading;
+
+export const selectUser = (state) => state.auth.userData;
+
+export const selectAuth = (state) => state.auth;
+
+export const selectApiKey = (state) => state.auth.apiKey;
+
+export const selectError = (state) => state.auth.error;
+
