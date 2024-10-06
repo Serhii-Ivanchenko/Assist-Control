08c5560701ad3269e801aa60d98465397a4192cf
JuliaPriadka <150530521+JuliaPriadka@users.noreply.github.com>
2024-10-06 16:47:26 +0300

    Update phone validation

--- src/validationSchemas/loginSchema.js
+++ src/validationSchemas/loginSchema.js
@@ -8,6 +8,5 @@ export const LoginSchema = Yup.object().shape({
     )
     .required("Поле Email повинно бути заповнене"),
   password: Yup.string()
-    .min(8, "Пароль повиннен бути не коротше за 8 символів")
     .required("Поле Пароль повинно бути заповнене"),
 });
