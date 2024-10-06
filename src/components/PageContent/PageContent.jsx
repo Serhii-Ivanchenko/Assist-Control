ef4ecb248dbf659a5eb829b169259b22d2eec420
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-10-06 14:31:01 +0300

    Update PageContent.jsx

--- src/components/PageContent/PageContent.jsx
+++ src/components/PageContent/PageContent.jsx
@@ -3,8 +3,8 @@ import LoadStatsChart from "../LoadStatsChart/LoadStatsChart";
 import CurrentCarsList from '../CurrentCarsList/CurrentCarsList.jsx'
 import Statistics from "../Statistics/Statistics";
 import VideoFrame from "../VideoFrame/VideoFrame";
-import CurrentCarsList from "../CurrentCarsList/CurrentCarsList";
 import css from "./PageContent.module.css";
+
 export default function PageContent() {
   return (
     <div className={css.pageContent}>
