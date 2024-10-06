7ec088a42c05548c131bce2ff1273bdb16bcefbc
Juliiaks <juliakharetonchuk@gmail.com>
2024-10-07 01:32:56 +0300

    added pictures to languages

--- src/components/Modals/UserSettingsModal/UserSettingsAccount/UserSettingsAccount.jsx
+++ src/components/Modals/UserSettingsModal/UserSettingsAccount/UserSettingsAccount.jsx
@@ -1,67 +1,12 @@
 import css from "./UserSettingsAccount.module.css";
 import { Formik, Field, Form } from "formik";
-import { IoIosArrowDown } from "react-icons/io";
 import { useId } from "react";
 import "/node_modules/flag-icons/css/flag-icons.min.css";
 import * as Yup from "yup";
 import { ErrorMessage } from "formik";
-// import { useField } from "formik";
-// import Select from "react-select"
 import { useState } from "react";
+import CustomSelect from "./CustomSelect/CustomSelect";
 
-const languages = [
-  { value: "ukr", label: "Українська", flag: "fi-ua" },
-  { value: "eng", label: "English", flag: "fi-gb" },
-];
-
-// const customStyles = {
-//   control: (provided) => ({
-//     ...provided,
-//     boxShadow: 'none',
-//     borderColor: '#302F2F',
-//     backgroundColor: '#282828',
-//     '&:hover': {
-//       borderColor: '#302F2F',
-//       backgroundColor: '#282828'
-//     },
-//   }),
-//   option: (provided, state) => ({
-//     ...provided,
-//     backgroundColor: state.isSelected ? '#f0f0f0' : '#fff',
-//     color: state.isSelected ? '#000' : '#333',
-//     padding: '10px',
-//     display: 'flex',
-//     alignItems: 'center', // Щоб прапорці вирівнювались з текстом
-//   }),
-//   singleValue: (provided) => ({
-//     ...provided,
-//     display: 'flex',
-//     alignItems: 'center', // Вирівнювання для вибраної опції
-//     color: '#333',
-//   }),
-// };
-
-// const FormikSelect = ({ label, options, name }) => {
-//   const [field, , helpers] = useField(name);
-//   const { value } = field;
-//   const { setValue } = helpers;
-
-//   const handleChange = (selectedOption) => {
-//     setValue(selectedOption.value);
-//   };
-
-//   return (
-//     <div>
-//       <label className={css.titles}>{label}</label>
-//       <Select 
-//         styles={css.inputSelect}
-//         options={options}
-//         value={options.find(option => option.value === value)}
-//         onChange={handleChange}
-//       />
-//     </div>
-//   );
-// };
 
 const initialValues = {
   company: '',
@@ -110,35 +55,18 @@ const handleSubmit = (values) => {
         </div>
 
       <label className={css.titles}>Пароль</label>
-      <button className={css.passwortChBtn}>Змінити пароль</button>
+      <button className={css.passwortChBtn} type="button">Змінити пароль</button>
 
       <label htmlFor={companyFieldId} className={css.titles}>Назва компанії</label>
         <Field type="text" name="company" className={css.inputs} id={companyFieldId} />
         <ErrorMessage name="company" component="span" className={css.errorMessage} />
 
-
-        {/* <FormikSelect
-          
-            name="languages"
-            label="Мова"
-            options={languages}
-          /> */}
-
         <label htmlFor={languagesFieldId} className={css.titles}>Мова</label>
 
-        <div className={css.selectWrapper}>
-
-      <Field as="select" name="languages" className={css.inputSelect} id={languagesFieldId}>
-           {languages.map((lang) => (
-              <option key={lang.value} value={lang.value}>
-                <span className={`fi ${lang.flag}`}></span> {lang.label}
-              </option>
-            ))}
-          </Field>
+          <Field name="languages" id={languagesFieldId} component={CustomSelect} />
           
-          <IoIosArrowDown className={css.selectIcon} />
-          <ErrorMessage name="languages" component="span" className={css.errorMessage} />
-        </div>
+          {/* <ErrorMessage name="languages" component="span" className={css.errorMessage} /> */}
+
 
       <div className={css.btnBox}>
         <button type="button" className={css.cancelBtn}>Відміна</button>
