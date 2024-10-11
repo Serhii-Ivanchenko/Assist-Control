c60b3b0847958e4b809bb34d83873fa77c0a9ff9
Valentine Ukho <petrova.hfhfhf@gmail.com>
2024-10-11 00:57:29 -1000

    adaptive 1920px and show car items in some date
    я закоментувала в компоненті Наталії те з чим працювала щоб отримувати дані, але поки не видаляла

--- src/components/DayCarsList/DayCarsList.jsx
+++ src/components/DayCarsList/DayCarsList.jsx
@@ -1,8 +1,6 @@
 import DayCarsItem from "../DayCarsItem/DayCarsItem.jsx";
 import styles from './DayCarsList.module.css';
 
-
-
 export default function DayCarsList({ carsData, hasDetailsButton }) {
   const maxVisibleCars = 3;
 
@@ -12,12 +10,12 @@ export default function DayCarsList({ carsData, hasDetailsButton }) {
         hasDetailsButton ? styles.withBtn : ""
       }`}
     >
-      {carsData.slice(0, maxVisibleCars).map((car, index) => (
+      {carsData.slice(0, maxVisibleCars).map((car) => (
         <DayCarsItem
-          key={index}
-          carNumber={car.number}
-          auto={car.model}
-          timeInfo={car.timeInfo}
+          key={car.id}
+          carNumber={car.plate}
+          auto={car.auto}
+          timeInfo={`Дата заїзду: ${new Date(car.date_s).toLocaleTimeString()}`}
         />
       ))}
     </div>
