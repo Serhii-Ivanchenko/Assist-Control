3b54722bdb659ba5eb4abcaae23af776e42d3abb
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-10-17 23:47:59 +0300

    Update PeriodSwitcher.jsx

--- src/components/PeriodSwitcher/PeriodSwitcher.jsx
+++ src/components/PeriodSwitcher/PeriodSwitcher.jsx
@@ -2,28 +2,31 @@ import clsx from "clsx";
 import css from "./PeriodSwitcher.module.css";
 import { useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
-// import { getCarsByDate, getCarsByMonth } from "../../redux/cars/operations";
 import { selectDate, selectDayCars } from "../../redux/cars/selectors";
 import { useEffect } from "react";
 import { getCarsByMonth } from "../../redux/cars/operations";
 import { useState } from "react";
+
 export default function PeriodSwitcher({ changeCarsArr }) {
   const [daySelected, setDaySelected] = useState(false);
+
   const day = useRef();
   const month = useRef();
+
   const activeClassName = clsx(css.statsBtn, css.activeBtn);
   const noneActiveClassName = clsx(css.statsBtn);
+
   const dayCars = useSelector(selectDayCars);
   const date = useSelector(selectDate);
   const selectedMonth = `${date.split("-")[0]}-${date.split("-")[1]}`;
-  console.log('====================================');
-  console.log(dayCars);
-  console.log('====================================');
+
   const dispatch = useDispatch();
+
   useEffect(() => {
     changeCarsArr(dayCars);
     handleChoseDay();
   }, [date, dayCars]);
+
   const handleChoseDay = () => {
     if (!daySelected || date !== new Date().toISOString().split("T")[0]) {
       month.current.className = noneActiveClassName;
@@ -32,10 +35,12 @@ export default function PeriodSwitcher({ changeCarsArr }) {
       setDaySelected(true);
     }
   };
+
   const handleChoseMonth = async () => {
     if (month.current.className === activeClassName) return;
     const { payload } = await dispatch(getCarsByMonth(selectedMonth));
     changeCarsArr(payload.cars);
+    setDaySelected(false);
     day.current.className = noneActiveClassName;
     month.current.className = activeClassName;
   };
