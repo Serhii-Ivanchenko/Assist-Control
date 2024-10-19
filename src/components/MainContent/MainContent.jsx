17606c7d171d255e44a7acc64798cc9b28a244bb
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-10-19 22:32:19 +0300

    layout changes

--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -5,7 +5,6 @@ import css from "./MainContent.module.css"
 export default function MainContent() {
   return (
     <div>
-      <h1 className={css.title}>Відеоконтроль</h1>
       <div className={css.mainContainer}>
       <PageContent />
       <CalendarBlock />
