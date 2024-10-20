2ec68b3a58f0ba715e86d01eeabe28336f7a9c7f
Valentine Ukho <petrova.hfhfhf@gmail.com>
2024-10-19 23:52:12 -1000

    chsnge button

--- src/components/DetailsBtn/DetailsBtn.jsx
+++ src/components/DetailsBtn/DetailsBtn.jsx
@@ -1,10 +1,11 @@
 import styles from './DetailsBtn.module.css';
+import { IoCarSportSharp } from "react-icons/io5";
 
 export default function DetailsBtn({ onClick }) {
   return (
     <div className={styles.btnContainer}>
       <button className={styles.btn} onClick={onClick}>
-      Детальна інформація
+      <p className={styles.btntext}>Всі авто</p><span> <IoCarSportSharp className={styles.iconAvto}/></span>
       </button>
     </div>
   );
