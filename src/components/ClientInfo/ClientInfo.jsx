01b0bbbf9f13221a8a104a1260314a2bdaf0b08e
Juliiaks <juliakharetonchuk@gmail.com>
2024-10-24 00:32:31 +0300

    added html

--- src/components/ClientInfo/ClientInfo.jsx
+++ src/components/ClientInfo/ClientInfo.jsx
@@ -4,6 +4,7 @@ import { BsDownload } from "react-icons/bs";
 import { BsTelephone } from "react-icons/bs";
 import { BsEnvelope } from "react-icons/bs";
 import { PiTelegramLogoLight } from "react-icons/pi";
+import { BsFiles } from "react-icons/bs";
 
 
 
@@ -13,7 +14,7 @@ import { PiTelegramLogoLight } from "react-icons/pi";
 export default function ClientInfo() {
     
     return (
-        <div>
+        <div className={css.clientInfoBox}>
             <p className={css.title}>Інформація про клієнта та автомобіль</p>
             
         <div className={css.infoBox}>
@@ -38,19 +39,45 @@ export default function ClientInfo() {
                 <ul className="">
                     <li className="">
                         <p className=""> <BsTelephone /> Tel</p>
-                        <div className="">+38 073 329 12 17</div>
+                        <input className="" value="+38 073 329 12 17"/>
                     </li>
                     <li className="">
                         <p className=""> <BsEnvelope /> E-mail</p>
-                        <div className="">ivan.petrenko@gmail.com</div>
+                        <input className="" value="ivan.petrenko@gmail.com"/>
                     </li>
                     <li className="">
                         <p className=""> <PiTelegramLogoLight /> Telegram</p>
-                        <p className="">ivan.petrenko</p>
+                        <input className="" value="ivan.petrenko"/>
                     </li>
                 </ul>
 
             </div>
+
+            <ul className={css.carInfo}>
+                <li className={css.carCard}>
+                    <img />
+                    <p className={css.carName}>HONDA CIVIC</p>
+                    <p className={css.carYear}>2001 </p>
+                    <ul>
+                        <li className="">
+                            <p>
+                                <span className={css.inBold}>VIN</span>
+                                <span className={css.number}>Number</span>
+                                <div>
+                                    <p>VW8795218794H46J</p>
+                                    <button><BsFiles /></button>
+                                </div>
+                            </p>
+                        </li>
+                        <li className="">
+                             <p>
+                                <span className={css.inBold}>CAR</span>
+                                <span className={css.number}>Number</span>
+                            </p>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
     </div>
 
     )
