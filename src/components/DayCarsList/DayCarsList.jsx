dd8c1dfbb5e431acab32ffd66a161f36ca8e0f72
Valentine Ukho <petrova.hfhfhf@gmail.com>
2024-10-27 16:09:31 -1000

    make style for crm

--- src/components/DayCarsList/DayCarsList.jsx
+++ src/components/DayCarsList/DayCarsList.jsx
@@ -10,23 +10,58 @@ export default function DayCarsList({ carsData, isModal, viewMode = "grid", isCR
 
   return (
     <div
-    className={clsx(
-      styles.dayCarsListContainer,  
-      isModal && styles.modaldayCarsListContainer,
-      isCRMBlock && styles.crmBlockDayCarsListContainer
-    )}
-  >
-    <div 
       className={clsx(
-        styles.scrollWrapper, 
-        isModal && styles.modalscrollWrapper,
-        isCRMBlock && styles.noScroll
+        styles.dayCarsListContainer,  
+        isModal && styles.modaldayCarsListContainer,
+        isCRMBlock && styles.crmBlockDayCarsListContainer
       )}
     >
+      {!isCRMBlock ? ( // Рендеримо scrollWrapper тільки якщо isCRMBlock відсутній
+        <div
+          className={clsx(
+            styles.scrollWrapper,
+            isModal && styles.modalscrollWrapper
+          )}
+        >
+          <ul 
+            className={clsx(
+              styles.carsList, 
+              isModal && styles.modalCarsList,
+              isCRMBlock && styles.crmCarList
+            )}
+          >
+            {visibleCars.map((car) => {
+              const CarItemComponent = viewMode === "list" ? DayCarsItemLine : DayCarsItem;
+
+              return (
+                <CarItemComponent
+                  className={styles.item}
+                  key={car.id}
+                  status={car.status}
+                  complete_d={car.complete_d}
+                  date_s={car.date_s}
+                  vin={car.vin}
+                  carNumber={car.plate}
+                  mileage={car.mileage}
+                  auto={car.auto}
+                  timeInfo={`Дата заїзду: ${new Date(car.date_s).toLocaleTimeString()}`}
+                  photoUrl={car.photo_url}
+                  isModal={isModal} 
+                  viewMode={viewMode}
+                  isCRMBlock={isCRMBlock}
+                  client={car.client}
+                />
+              );
+            })}
+          </ul>
+        </div>
+      ) : (
+        // Якщо isCRMBlock, то scrollWrapper не рендериться
         <ul 
           className={clsx(
             styles.carsList, 
-            isModal && styles.modalCarsList
+            isModal && styles.modalCarsList,
+            isCRMBlock && styles.crmCarList
           )}
         >
           {visibleCars.map((car) => {
@@ -49,12 +84,11 @@ export default function DayCarsList({ carsData, isModal, viewMode = "grid", isCR
                 viewMode={viewMode}
                 isCRMBlock={isCRMBlock}
                 client={car.client}
-                
               />
             );
           })}
         </ul>
-      </div>
+      )}
     </div>
   );
 }
