32e64f72d3de775cf838175c1736ab3b0005f795
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-11-03 16:13:56 +0200

    responsive sidebar done

--- src/components/PremiumAd/PremiumAd.jsx
+++ src/components/PremiumAd/PremiumAd.jsx
@@ -4,15 +4,14 @@ import { FaCrown } from "react-icons/fa";
 export default function PremiumAd() {
   return (
     <div className={styles.premiumContainer}>
-      <FaCrown className={styles.premiumIcon} />
-      <div className={styles.textBlock}>
-        <h3 className={styles.premiumTitle}>Преміум</h3>
-        <p className={styles.premiumDescription}>
-          Ще більше різних можливостей <br /> для Вас!
-        </p>
-      </div>
-
-      <button className={styles.premiumButton}>Перейти</button>
+        <FaCrown className={styles.premiumIcon} />
+        <div className={styles.textBlock}>
+          <h3 className={styles.premiumTitle}>Преміум</h3>
+          <p className={styles.premiumDescription}>
+            Ще більше різних можливостей <br /> для Вас!
+          </p>
+        </div>
+        <button className={styles.premiumButton}>Перейти</button>
     </div>
   );
 }
