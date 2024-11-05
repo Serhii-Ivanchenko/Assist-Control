c69bf7410cfbf25823435dd3a934fcd7e5bb6462
Serhii Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-11-05 11:54:23 +0200

    comment div.wrapper

--- src/components/ServiceStationItem/ServiceStationItemItem.jsx
+++ src/components/ServiceStationItem/ServiceStationItemItem.jsx
@@ -33,104 +33,101 @@ function ServiceStationItem({ name, isOpen, onToggle }) {
   return (
     <div className={styles.generalContainer}>
       {isOpen ? (
-        <div className={styles.wrapper} onClick={onToggle}>
-          <div
-            className={styles.serviceWrapper}
-            onClick={(e) => e.stopPropagation()}
-          >
-            <div className={styles.serviceContainer}>
-              <h3 className={styles.serviceTitle}>{name}</h3>
-              <button
-                className={styles.editBtn}
-                onClick={(e) => {
-                  e.stopPropagation();
-                  handleEditToggle();
-                }}
-              >
-                <BsPencil className={styles.mainIcon} />
-              </button>
-            </div>
-
-            <ul className={styles.infoList}>
-              <li className={styles.infoItem}>
-                <div className={styles.infoKey}>
-                  <BsEnvelope className={styles.keyIcon} />
-                  <p>Email</p>
-                </div>
-                {isEdit ? (
-                  <input
-                    type="text"
-                    name="email"
-                    value={info.email}
-                    onChange={handleInputChange}
-                    className={styles.infoValueInput}
-                  />
-                ) : (
-                  <p className={styles.infoValue}>{info.email}</p>
-                )}
-              </li>
-              <li className={styles.infoItem}>
-                <p className={styles.infoKey}>
-                  <BsGeoAlt className={styles.keyIcon} /> Адреса
-                </p>
-                {isEdit ? (
-                  <input
-                    type="text"
-                    name="address"
-                    value={info.address}
-                    onChange={handleInputChange}
-                    className={styles.infoValueInput}
-                  />
-                ) : (
-                  <p className={styles.infoValue}>{info.address}</p>
-                )}
-              </li>
-              <li className={styles.infoItem}>
-                <p className={styles.infoKey}>
-                  <BsCreditCard2Back className={styles.keyIcon} /> Статус
-                  підписки:
-                </p>
-                <div className={styles.subscribeStatus}>
-                  <BsCreditCard2Back />
-                  {info.subscriptionStatus}
-                </div>
-              </li>
-              <li className={styles.infoItem}>
-                <p className={styles.infoKey}>
-                  <BsPeople className={styles.keyIcon} /> Співробітники
-                </p>
-                {isEdit ? (
-                  <input
-                    type="text"
-                    name="employees"
-                    value={info.employees}
-                    onChange={handleInputChange}
-                    className={styles.infoValueInput}
-                  />
-                ) : (
-                  <p className={styles.infoValue}>{info.employees}</p>
-                )}
-              </li>
-              <li className={styles.infoItem}>
-                <p className={styles.infoKey}>
-                  <BsCalendarWeek className={styles.keyIcon} /> Дійсний до:
-                </p>
-                {isEdit ? (
-                  <input
-                    type="text"
-                    name="validUntil"
-                    value={info.validUntil}
-                    onChange={handleInputChange}
-                    className={styles.infoValueInput}
-                  />
-                ) : (
-                  <p className={styles.infoValue}>{info.validUntil}</p>
-                )}
-              </li>
-            </ul>
+        // <div className={styles.wrapper} onClick={onToggle}>
+        <div className={styles.serviceWrapper} onClick={onToggle}>
+          <div className={styles.serviceContainer}>
+            <h3 className={styles.serviceTitle}>{name}</h3>
+            <button
+              className={styles.editBtn}
+              onClick={(e) => {
+                e.stopPropagation();
+                handleEditToggle();
+              }}
+            >
+              <BsPencil className={styles.mainIcon} />
+            </button>
           </div>
+
+          <ul className={styles.infoList}>
+            <li className={styles.infoItem}>
+              <div className={styles.infoKey}>
+                <BsEnvelope className={styles.keyIcon} />
+                <p>Email</p>
+              </div>
+              {isEdit ? (
+                <input
+                  type="text"
+                  name="email"
+                  value={info.email}
+                  onChange={handleInputChange}
+                  className={styles.infoValueInput}
+                />
+              ) : (
+                <p className={styles.infoValue}>{info.email}</p>
+              )}
+            </li>
+            <li className={styles.infoItem}>
+              <p className={styles.infoKey}>
+                <BsGeoAlt className={styles.keyIcon} /> Адреса
+              </p>
+              {isEdit ? (
+                <input
+                  type="text"
+                  name="address"
+                  value={info.address}
+                  onChange={handleInputChange}
+                  className={styles.infoValueInput}
+                />
+              ) : (
+                <p className={styles.infoValue}>{info.address}</p>
+              )}
+            </li>
+            <li className={styles.infoItem}>
+              <p className={styles.infoKey}>
+                <BsCreditCard2Back className={styles.keyIcon} /> Статус
+                підписки:
+              </p>
+              <div className={styles.subscribeStatus}>
+                <BsCreditCard2Back />
+                {info.subscriptionStatus}
+              </div>
+            </li>
+            <li className={styles.infoItem}>
+              <p className={styles.infoKey}>
+                <BsPeople className={styles.keyIcon} /> Співробітники
+              </p>
+              {isEdit ? (
+                <input
+                  type="text"
+                  name="employees"
+                  value={info.employees}
+                  onChange={handleInputChange}
+                  className={styles.infoValueInput}
+                />
+              ) : (
+                <p className={styles.infoValue}>{info.employees}</p>
+              )}
+            </li>
+            <li className={styles.infoItem}>
+              <p className={styles.infoKey}>
+                <BsCalendarWeek className={styles.keyIcon} /> Дійсний до:
+              </p>
+              {isEdit ? (
+                <input
+                  type="text"
+                  name="validUntil"
+                  value={info.validUntil}
+                  onChange={handleInputChange}
+                  className={styles.infoValueInput}
+                />
+              ) : (
+                <p className={styles.infoValue}>{info.validUntil}</p>
+              )}
+            </li>
+          </ul>
         </div>
       ) : (
+        // </div>
         <div className={styles.closedServiceWrapper} onClick={onToggle}>
           <div className={styles.closedServiceContainer}>
             <h3 className={styles.serviceTitle}>{name}</h3>
