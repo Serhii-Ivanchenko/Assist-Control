02497c224b124d37367d0e7c9c1681cac0e68715
Volodymyr <palcinskijvolodimir173@gmail.com>
2024-11-05 21:56:14 +0200

    create detail record info block

--- src/components/ServiceHistory/ItemOfRecord/ItemOfRecord.jsx
+++ src/components/ServiceHistory/ItemOfRecord/ItemOfRecord.jsx
@@ -2,13 +2,17 @@ import { SlSpeedometer } from "react-icons/sl";
 import {
   BsCalendar2Week,
   BsCaretDownFill,
-  BsDownload,
   BsPlayFill,
   BsChevronDown,
   BsFilter,
   BsRecordCircle,
-  BsSearch,
-  BsXLg,
+  BsWrench,
+  BsUiChecksGrid,
+  BsShieldExclamation,
+  BsReceipt,
+  // BsDownload,
+  // BsSearch,
+  // BsXLg,
 } from "react-icons/bs";
 import { GiSoundWaves } from "react-icons/gi";
 import Accordion from "@mui/material/Accordion";
@@ -18,16 +22,41 @@ import userAvater from "../../../assets/images/ava.png";
 import TranscriptionMessage from "../TranscriptionMessage/TranscriptionMessage";
 import css from "./ItemOfRecord.module.css";
 import { useState } from "react";
-import Modal from "../../Modals/Modal/Modal.jsx";
 import clsx from "clsx";
+import RecordBtnInfo from "../RecordBtnInfo/RecordBtnInfo";
 
 export default function ItemOfRecord({ item, messages, isExpanded }) {
-  const [apealModal, setApealModal] = useState(false);
   const [showDialogModal, setShowDialogModal] = useState(false);
+
   const [transcription, setTranscription] = useState(false);
-  const toogleApealModal = () => setApealModal(!apealModal);
+
+  const [recordInfo, setRecordInfo] = useState("appeal");
+
+  const [isRecordInfoOpen, setIsRecordInfoOpen] = useState(
+    Boolean(item.appeal)
+  );
+
   const toogleTranscription = () => setTranscription(!transcription);
+
   const toogleDialogModal = () => setShowDialogModal(!showDialogModal);
+
+  const handleToogleRecordInfo = (value) => {
+    value ? setRecordInfo(value) : setRecordInfo("");
+    if (value) {
+      setIsRecordInfoOpen(true);
+      setRecordInfo(value);
+    } else {
+      setRecordInfo("");
+      // const timeout = setTimeout(() => {
+      //   setIsRecordInfoOpen(false);
+      // }, 300);
+      // clearTimeout(timeout);
+    }
+  };
+  const handleSetRecordInfo = (string) =>
+    recordInfo === string
+      ? handleToogleRecordInfo()
+      : handleToogleRecordInfo(string);
   return (
     <li key={item.totalkilometrs} className={css.itemOfAccardion}>
       <div className={css.itemOfMarking}>
@@ -60,10 +89,8 @@ export default function ItemOfRecord({ item, messages, isExpanded }) {
               </div>
               <div className={css.dateWrapper}>
                 <div className={css.date}>
-
                   <BsCalendar2Week /> <div>{item.date}</div>
                   <div>{item.time}</div>
-
                 </div>
                 <button
                   className={clsx(
@@ -83,131 +110,155 @@ export default function ItemOfRecord({ item, messages, isExpanded }) {
             padding: "0",
           }}
         >
-          {
-            <div className={css.bodyAccardionWrapper}>
-              <div className={css.btnDownloadsWrapper}>
-                <div className={css.btnDownloadsItem}>
-                  <p>Діагностика</p>
-                  <button className={css.downloadBtn}>
-                    <BsDownload />
-                    <p>.pdf</p>
-                  </button>
+          <div className={css.bodyAccardionWrapper}>
+            <div className={css.btnDownloadsWrapper}>
+              <button
+                className={css.btnDownloadsItem}
+                onClick={() => handleSetRecordInfo("appeal")}
+                style={
+                  item.appeal && recordInfo === "appeal"
+                    ? { cursor: "pointer", outline: "1px solid #fff" }
+                    : item.appeal
+                    ? null
+                    : { cursor: "pointer" }
+                }
+                disabled={!item.appeal}
+              >
+                <p>Звернення</p>
+                <div
+                  className={clsx(css.downloadBtn, item.appeal && css.btnBg)}
+                >
+                  <BsReceipt size={13} />
                 </div>
-                <div className={css.btnDownloadsItem}>
-                  <p>Ремонт</p>
-                  <button className={css.downloadBtn}>
-                    <BsDownload />
-                    <p>.pdf</p>
-                  </button>
+              </button>
+              <button
+                className={css.btnDownloadsItem}
+                onClick={() => handleSetRecordInfo("diagnostic")}
+                style={
+                  item.diagnostic && recordInfo === "diagnostic"
+                    ? { cursor: "pointer", outline: "1px solid #fff" }
+                    : !item.diagnostic
+                    ? null
+                    : { cursor: "pointer" }
+                }
+                disabled={!item.diagnostic}
+              >
+                <p>Діагностика</p>
+                <div
+                  className={clsx(
+                    css.downloadBtn,
+                    item.diagnostic && css.btnBg
+                  )}
+                >
+                  <BsUiChecksGrid size={13} />
                 </div>
-                <div className={css.btnDownloadsItem}>
-                  <p>Рекомендації</p>
-                  <button className={css.downloadBtn}>
-                    <BsDownload />
-                    <p>.pdf</p>
-                  </button>
+              </button>
+              <button
+                className={css.btnDownloadsItem}
+                onClick={() => handleSetRecordInfo("repair")}
+              >
+                <p>Ремонт</p>
+                <div className={css.downloadBtn}>
+                  <BsWrench size={13} />
+                </div>
+              </button>
+              <button
+                className={css.btnDownloadsItem}
+                onClick={() => handleSetRecordInfo("recommendation")}
+              >
+                <p>Рекомендації</p>
+                <div className={css.downloadBtn}>
+                  <BsShieldExclamation size={13} />
+                </div>
+              </button>
+            </div>
+            {isRecordInfoOpen ? (
+              <div>
+                <div
+                  className={clsx(
+                    css.RecordBtnInfoWrapper,
+                    recordInfo
+                      ? css.openRecordBtnInfoWrapper
+                      : css.closeRecordBtnInfoWrapper
+                  )}
+                >
+                  <RecordBtnInfo recordInfo={recordInfo} item={item} />
                 </div>
               </div>
-              <div className={css.secondAcordionList}>
-                <div className={css.btnDownloadsItem}>
-                  <p>Звернення</p>
-                  <button
-                    className={css.downloadBtn}
-                    onClick={() => toogleApealModal()}
-                  >
-                    <BsSearch />
-                  </button>
-                  <Modal isOpen={apealModal} onClose={toogleApealModal}>
-                    <div className={css.apealModalWrapper}>
-                      <BsXLg
-                        className={css.closeIcon}
-                        onClick={() => toogleApealModal()}
-                      />
-                      <p>
-                        Привіт! Мене звати [Ім`я], і я хочу записатися на ремонт
-                        свого автомобіля. У мене [марка і модель авто], і після
-                        нещодавньої аварії потрібен огляд і ремонт кузова,
-                        зокрема вирівнювання геометрії та заміна пошкоджених
-                        деталей. Також цікавить діагностика стану автомобіля
-                        після ремонту. Чи є у вас вільні дати на цьому тижні,
-                        щоб я міг під`їхати на оцінку? Дякую!
-                      </p>
-                    </div>
-                  </Modal>
-                </div>
-                <div className={css.secondAcordionWrapper}>
-                  <Accordion
-                    disableGutters={true}
-                    sx={{
-                      background: "none",
-                      color: "inherit",
-                      WebkitBoxShadow: "none",
-                    }}
-                  >
-                    <div className={css.secondAcordion}>
-                      <div className={css.callRecordWrapper}>
-                        <img src={userAvater} alt="" />
-                        <div className={css.callRecord}>
-                          <div>
-                            <BsPlayFill
-                              size={26}
-                              fill="var(--play-btn-triangle)"
-                            />
-                          </div>
-                          <GiSoundWaves size={80} />
-                          <p>1:26</p>
-                        </div>
-                      </div>
-                      <AccordionSummary
-                        sx={{
-                          padding: "0",
-                        }}
-                      >
-                        <button
-                          className={css.transcriptionToggleBtn}
-                          onClick={() => toogleTranscription()}
-                        >
-                          <p>Aa</p>
-                          <BsChevronDown
-                            className={clsx(
-                              css.transcriptionIcon,
-                              transcription ? css.activeTranscriptionIcon : null
-                            )}
-                            strokeWidth={3}
-                            size={14}
+            ) : null}
+            <div className={css.secondAcordionList}>
+              <div className={css.secondAcordionWrapper}>
+                <Accordion
+                  disableGutters={true}
+                  sx={{
+                    background: "none",
+                    color: "inherit",
+                    WebkitBoxShadow: "none",
+                  }}
+                >
+                  <div className={css.secondAcordion}>
+                    <div className={css.callRecordWrapper}>
+                      <img src={userAvater} alt="" />
+                      <div className={css.callRecord}>
+                        <div>
+                          <BsPlayFill
+                            size={24}
+                            fill="var(--play-btn-triangle)"
                           />
-                        </button>
-                      </AccordionSummary>
+                        </div>
+                        <GiSoundWaves size={80} />
+                        <p>1:26</p>
+                      </div>
                     </div>
-                    <AccordionDetails
+                    <AccordionSummary
                       sx={{
                         padding: "0",
                       }}
                     >
-                      <div className={css.scrollBarWrapper}>
-                        <div className={css.secondAcordionBody}>
-                          <button className={css.transcriptionImportantBtn}>
-                            <BsFilter />
-                            <p>Головне</p>
-                          </button>
-                          <ul className={css.messages}>
-                            {messages.map(({ orClientMsg, time, message }) => (
-                              <TranscriptionMessage
-                                key={Math.random()}
-                                orClientMsg={orClientMsg}
-                                time={time}
-                                message={message}
-                              />
-                            ))}
-                          </ul>
-                        </div>
+                      <button
+                        className={css.transcriptionToggleBtn}
+                        onClick={() => toogleTranscription()}
+                      >
+                        <p>Aa</p>
+                        <BsChevronDown
+                          className={clsx(
+                            css.transcriptionIcon,
+                            transcription ? css.activeTranscriptionIcon : null
+                          )}
+                          strokeWidth={3}
+                          size={14}
+                        />
+                      </button>
+                    </AccordionSummary>
+                  </div>
+                  <AccordionDetails
+                    sx={{
+                      padding: "0",
+                    }}
+                  >
+                    <div className={css.scrollBarWrapper}>
+                      <div className={css.secondAcordionBody}>
+                        <button className={css.transcriptionImportantBtn}>
+                          <BsFilter />
+                          <p>Головне</p>
+                        </button>
+                        <ul className={css.messages}>
+                          {messages.map(({ orClientMsg, time, message }) => (
+                            <TranscriptionMessage
+                              key={Math.random()}
+                              orClientMsg={orClientMsg}
+                              time={time}
+                              message={message}
+                            />
+                          ))}
+                        </ul>
                       </div>
-                    </AccordionDetails>
-                  </Accordion>
-                </div>
+                    </div>
+                  </AccordionDetails>
+                </Accordion>
               </div>
             </div>
-          }
+          </div>
         </AccordionDetails>
       </Accordion>
     </li>
