d2a1b9a2ff859baa534cc905ea40009cd3b7d297
JuliaPriadka <150530521+JuliaPriadka@users.noreply.github.com>
2024-11-07 16:37:52 +0200

    Updated ServiceBookingModal css, added buttons to switch dates

--- src/components/Modals/ServiceBookingModal/SelectDate/SelectDate.jsx
+++ src/components/Modals/ServiceBookingModal/SelectDate/SelectDate.jsx
@@ -1,33 +1,62 @@
-import { forwardRef } from "react";
+import { forwardRef, useEffect, useState } from "react";
 import css from "../SelectDate/SelectDate.module.css";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { BsCalendar2Week } from "react-icons/bs";
+import { BsChevronLeft } from "react-icons/bs";
+import { BsChevronRight } from "react-icons/bs";
+import { format, addDays, subDays } from "date-fns";
 
 export default function SelectDate({ newDate }) {
-  const CustomInput = forwardRef(({ onClick }, ref) => (
-    <button
-      type="button"
-      className={css.customInput}
-      onClick={onClick}
-      ref={ref}
-    >
-      <BsCalendar2Week />
-      Обрати дату
-    </button>
+  const [startDate, setStartDate] = useState(new Date());
+
+  useEffect(() => {
+    newDate(startDate.toLocaleDateString());
+  }, [newDate, startDate]);
+
+  const CustomInput = forwardRef(({ value, onClick }, ref) => (
+    <div className={css.datePickerWrapper}>
+      <button
+        type="button"
+        className={css.datePaginationBtn}
+        onClick={() => {
+          setStartDate(subDays(value, 1));
+        }}
+        disabled={new Date(value) <= new Date()}
+      >
+        <BsChevronLeft className={css.btnIcon} />
+      </button>
+      <div className={css.dateWrapper}>
+        <button
+          type="button"
+          className={css.customInput}
+          onClick={onClick}
+          ref={ref}
+        >
+          <BsCalendar2Week />
+        </button>
+        <p>{format(value, "dd.MM.yyyy")}</p>
+      </div>
+      <button
+        type="button"
+        className={css.datePaginationBtn}
+        onClick={() => setStartDate(addDays(value, 1))}
+      >
+        <BsChevronRight className={css.btnIcon} />
+      </button>
+    </div>
   ));
 
   CustomInput.displayName = "CustomInput";
 
   return (
-    <div className={css.wrapper}>
+    <>
       <DatePicker
-        selected={Date.now()}
-        onChange={(date) => newDate(date.toLocaleDateString())}
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
         minDate={new Date()}
         customInput={<CustomInput />}
-        dateFormat="dd/MM/yyyy"
       />
-    </div>
+    </>
   );
 }
