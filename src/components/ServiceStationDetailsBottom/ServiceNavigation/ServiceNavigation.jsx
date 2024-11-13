7510d67df653374e1657dc3a106b99d24aea9454
Juliiaks <juliakharetonchuk@gmail.com>
2024-11-14 01:28:46 +0200

    added one more btn

--- src/components/ServiceStationDetailsBottom/ServiceNavigation/ServiceNavigation.jsx
+++ src/components/ServiceStationDetailsBottom/ServiceNavigation/ServiceNavigation.jsx
@@ -1,7 +1,15 @@
 import css from "./ServiceNavigation.module.css"
 import clsx from "clsx"
+import { BsChevronRight, BsChevronLeft  } from "react-icons/bs";
+
+
+
 export default function ServiceNavigation({page, setPage}) {
     return (
+        <div className={css.navSettings}>
+
+            <span className={css.arrowBtn}> <BsChevronLeft className={css.arrow } size={18}/> </span>
+            
         <div className={css.btnBox}>
             <button className={css.btnService}>
                 <p className={css.title}>Камери</p>
@@ -22,15 +30,23 @@ export default function ServiceNavigation({page, setPage}) {
             <button type="button"
                 onClick={()=>setPage("works")}
                 className={clsx(css.btnService, { [css.isActiveBtn]: page === "works" })}>
-                <p className={css.title}>Роботи</p>
-                <p className={`${css.value} ${css.valueWords}`}>ПРАЙС</p>
+                <p className={css.title}>ПРАЙС</p>
+                <p className={`${css.value} ${css.valueWords}`}>Роботи</p>
             </button>
             <button type="button"
                 onClick={()=>setPage("spares")}
                 className={clsx(css.btnService, { [css.isActiveBtn]: page === "spares" })}>
-                <p className={css.title}>Запчастини</p>
-                <p className={`${css.value} ${css.valueWords}`}>НАЦІНКА</p>
+                <p className={css.title}>НАЦІНКА</p>
+                <p className={`${css.value} ${css.valueWords}`}>Запчастини</p>
             </button>
+             <button type="button"
+                onClick={()=>setPage("warehouse")}
+                className={clsx(css.btnService, { [css.isActiveBtn]: page === "warehouse" })}>
+                <p className={css.title}>СКЛАД</p>
+                {/* <p className={`${css.value} ${css.valueWords}`}>НАЦІНКА</p> */}
+            </button>
+            </div>
+             <span className={css.arrowBtn}> <BsChevronRight className={css.arrow } size={18}/> </span>
         </div>
     )
 }
