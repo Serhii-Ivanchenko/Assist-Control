ba2d4b7a2b2cb77f69320d59b02f1dc030824540
Juliiaks <juliakharetonchuk@gmail.com>
2024-11-16 14:02:10 +0200

    updated checkbox

--- src/components/ServiceStationDetailsBottom/SparesPart/SparesPart.jsx
+++ src/components/ServiceStationDetailsBottom/SparesPart/SparesPart.jsx
@@ -1,8 +1,12 @@
 import css from "./SparesPart.module.css"
-import { BsCheckSquare } from "react-icons/bs";
-import CustomRadioBtn from "../../CustomRadioBtn/CustomRadioBtn.jsx"
+// import { BsCheckSquare } from "react-icons/bs";
+// import CustomRadioBtn from "../../CustomRadioBtn/CustomRadioBtn.jsx"
 import { BsPlusLg } from "react-icons/bs";
 import { BsDashSquareFill } from "react-icons/bs";
+import { BsCheckLg  } from "react-icons/bs";
+import { BsCheck } from "react-icons/bs";
+import { BsRecordFill } from "react-icons/bs";
+
 
  
 
@@ -13,25 +17,29 @@ export default function SparesPart() {
             <div className={css.cbPartBox}>
                 <label className={css.cbLabel}>
                     <input type="checkbox" className={css.checkbox}></input>
-                    <span className={css.cbMark}><BsCheckSquare size={24} className={css.cbIcon} /></span>
+                    <span className={css.cbMark}><BsCheck size={24} className={css.cbIcon} /></span>
                     Загальна
                 </label>
 
                 <label className={css.cbLabel}>
                     <input type="checkbox" className={css.checkbox}></input>
-                    <span className={css.cbMark}><BsCheckSquare size={24} className={css.cbIcon}/></span>
+                    <span className={css.cbMark}>
+                        {/* <BsCheck size={24} className={css.cbIcon} /> */}
+                    </span>
                     Постачальник
                 </label>
 
                 <label className={css.cbLabel}>
                     <input type="checkbox" className={css.checkbox}></input>
-                    <span className={css.cbMark}><BsCheckSquare size={24} className={css.cbIcon}/></span>
+                    <span className={css.cbMark}>
+                        {/* <BsCheck size={24} className={css.cbIcon} /> */}
+                    </span>
                     Бренд
                 </label>
 
                 <label className={css.cbLabel}>
                     <input type="checkbox" className={css.checkbox}></input>
-                    <span className={css.cbMark}><BsCheckSquare size={24} className={css.cbIcon}/></span>
+                    <span className={css.cbMark}><BsCheck size={24} className={css.cbIcon}/></span>
                     Група
                 </label>
             </div>
@@ -41,13 +49,13 @@ export default function SparesPart() {
             <div className={css.radioBtns}>
                 <label className={css.radioLabel}>
                     <input type="radio" className={css.radiobutton } />
-                    <CustomRadioBtn />
+                        <span className={css.radiospan}> <BsRecordFill size={15} className={css.dotIcon} /> </span>
                     Фіксована націнка (%)
                 </label>
 
                 <label className={css.radioLabel}>
                     <input type="radio" className={css.radiobutton }/>
-                    <CustomRadioBtn />
+                    <span className={css.radiospan}> <BsRecordFill size={12} className={css.dotIcon}/> </span>
                     Динамічна націнка (%)
                 </label>
             </div>
@@ -113,7 +121,10 @@ export default function SparesPart() {
 
                         </ul>
 
-               
+                        <div className={css.btnBox}>
+                            <button type="button" className={css.cancel}>Закрити</button>
+                            <button type="button" className={css.save}> <BsCheckLg size={18}/> Зберегти</button>
+               </div>
                         
                         
                     </div>
