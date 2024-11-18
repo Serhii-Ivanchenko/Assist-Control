82d18cb63704c26f423ea09d4595bf2e8ca58b87
Juliiaks <juliakharetonchuk@gmail.com>
2024-11-18 13:06:46 +0200

    started adding of warehouse

--- src/components/ServiceStationDetailsBottom/WarehousePart/WarehousePart.jsx
+++ src/components/ServiceStationDetailsBottom/WarehousePart/WarehousePart.jsx
@@ -1,7 +1,55 @@
+import css from "./WarehousePart.module.css"
+import { RiDatabaseLine } from "react-icons/ri";
+import { RiFridgeLine } from "react-icons/ri";
+import { RiTableAltLine } from "react-icons/ri";
+import { RiFolder5Line } from "react-icons/ri";
+import { BiBuildingHouse } from "react-icons/bi";
+import { BsFolderPlus, BsThreeDotsVertical  } from "react-icons/bs";
+
+
+
+
+
 export default function WarehousePart() {
     return (
         <div>
-            <p>WarehousePart</p>
+
+            <div className={css.listAndButton}>
+            <ul className={css.itemsList}>
+                <li className={css.items}>
+                    <BiBuildingHouse/>
+                    <p>6</p>
+                    <p>Склади</p>
+                </li>
+                <li className={css.items}>
+                    <RiDatabaseLine />
+                    <p>14</p>
+                    <p>Секції</p>
+                </li>
+                <li className={css.items}>
+                    <RiFridgeLine />
+                    <p>46</p>
+                    <p>Стелажі</p>
+                </li>
+                <li className={css.items}>
+                    <RiTableAltLine/>
+                    <p>94</p>
+                    <p>Полиці</p>
+                </li>
+                <li className={css.items}>
+                    <RiFolder5Line/>
+                    <p>116</p>
+                    <p>Місця</p>
+                </li>
+            </ul>
+
+                <button type="button" className={css.newWarehouse}>
+                    <BsFolderPlus/>
+                    Новий склад
+                    <BsThreeDotsVertical />
+                </button>
+            </div>
+
         </div>
     )
 }
