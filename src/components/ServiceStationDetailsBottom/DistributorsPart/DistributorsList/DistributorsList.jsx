75be9a687969de3a0317d38be6df3db4c6b070b0
Anastasiiya Onyshchuk <shapoval.anastasiiya@gmail.com>
2024-11-26 20:14:20 +0200

    fix unique id in inputs

--- src/components/ServiceStationDetailsBottom/DistributorsPart/DistributorsList/DistributorsList.jsx
+++ src/components/ServiceStationDetailsBottom/DistributorsPart/DistributorsList/DistributorsList.jsx
@@ -1,13 +1,23 @@
 import DistributorsCard from "../DistributorsCard/DistributorsCard";
 import styles from "./DistributorsList.module.css";
 
+const distributorsData = [
+  { id: 1, name: "Distributor 1" },
+  { id: 2, name: "Distributor 2" },
+  { id: 3, name: "Distributor 3" },
+  { id: 4, name: "Distributor 4" },
+];
+
 function DistributorsList() {
   return (
     <div className={styles.wrapper}>
-      <DistributorsCard />
-      <DistributorsCard />
-      <DistributorsCard />
-      <DistributorsCard />
+      {distributorsData.map((distributor) => (
+        <DistributorsCard
+          key={distributor.id}
+          id={distributor.id}
+          name={distributor.name}
+        />
+      ))}
     </div>
   );
 }
