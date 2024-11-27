de9c7cc75244966f4b2afb767318d8413894d8a7
Juliiaks <juliakharetonchuk@gmail.com>
2024-11-28 01:31:24 +0200

    changed addModal title

--- src/components/ServiceStationDetailsBottom/WarehousePart/AddModal/AddModal.jsx
+++ src/components/ServiceStationDetailsBottom/WarehousePart/AddModal/AddModal.jsx
@@ -10,10 +10,10 @@ export default function AddModal() {
 
   return (
     <div className={css.modalBox}>
-      <p className={css.modalTitle}>Введіть назву шаблона</p>
-      <Formik initialValues={{ newWarehouse: "" }} onSubmit={handleSubmit}>
+      <p className={css.modalTitle}>Введіть кількість</p>
+      <Formik initialValues={{ number: "" }} onSubmit={handleSubmit}>
         <Form className={css.form}>
-          <Field name="newWarehouse" className={css.field} />
+          <Field name="number" className={css.field} />
           <div className={css.btnBox}>
             <button type="button" className={css.btnClose}>
               Закрити
