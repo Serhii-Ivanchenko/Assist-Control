d49e73b9ff2e21f755c252974a482a646f78caac
Juliiaks <juliakharetonchuk@gmail.com>
2024-12-04 03:37:46 +0200

    Update UploadComponent.jsx

--- src/components/sharedComponents/UploadComponent/UploadComponent.jsx
+++ src/components/sharedComponents/UploadComponent/UploadComponent.jsx
@@ -1,15 +1,67 @@
 import css from "./UploadComponent.module.css";
-import { Field } from "formik";
 import { BsFillCloudUploadFill } from "react-icons/bs";
+import { useRef } from "react";
 
-export default function UploadComponent({ title, name }) {
+export default function UploadComponent({ title, name, setAvatar }) {
+  const fileInputRef = useRef(null);
+
+  const handleChangePhoto = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  const handleFileChange = async (event) => {
+    const file = event.currentTarget.files[0];
+    if (file) {
+      const newAvatarUrl = URL.createObjectURL(file);
+      setAvatar(newAvatarUrl);
+      // try {
+      //   await dispatch(updateUserAvatar(file)).unwrap();
+
+      //   toast.success("Аватар успішно оновлено :)", {
+      //     position: "top-right",
+      //     duration: 5000,
+      //     style: {
+      //       background: "var(--bg-input)",
+      //       color: "var(--white)FFF",
+      //     },
+      //   });
+      // } catch (error) {
+      //   setAvatar(userPhoto);
+      //   console.error("Помилка при оновленні аватара", error);
+      //   toast.error("Не вдалося оновити аватар :(", {
+      //     position: "top-right",
+      //     duration: 5000,
+      //     style: {
+      //       background: "var(--bg-input)",
+      //       color: "var(--white)FFF",
+      //     },
+      //   });
+      // }
+    }
+  };
+
   return (
     <div>
       <label className={`${css.docLabel} ${css.docLabelForPhoto}`}>
         {" "}
-        <BsFillCloudUploadFill className={css.icon} /> {title}
+        <BsFillCloudUploadFill
+          className={css.icon}
+          onClick={handleChangePhoto}
+        />{" "}
+        {title}
       </label>
-      <Field type="file" name={name} className={css.docInput} />
+      <input
+        type="file"
+        name={name}
+        className={css.docInput}
+        ref={fileInputRef}
+        onChange={handleFileChange}
+        multiple
+        accept="image/*"
+      />
+      {/* <Field type="file" name={name} className={css.docInput} /> */}
     </div>
   );
 }
