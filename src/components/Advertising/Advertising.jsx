4ef2848b5ac9b3a87e221187f1c03e9b2bbd96ce
JuliaPriadka <150530521+JuliaPriadka@users.noreply.github.com>
2024-12-06 17:12:05 +0200

    Create Advertising component

---
+++ src/components/Advertising/Advertising.jsx
@@ -0,0 +1,25 @@
+import css from "../Advertising/Advertising.module.css";
+import { BsBoxArrowUpRight } from "react-icons/bs";
+
+export default function Advertising() {
+  return (
+    <div className={css.wrapper}>
+      <div className={css.imgWrapper}></div>
+      <div className={css.textWrapper}>
+        <p className={css.header}>Повне керівництво по сервісу CRMmech</p>
+        <p className={css.text}>
+          Якщо ви тільки почали знайомство, рекомендуємо пройти безкоштоввний
+          відеотренінг.
+        </p>
+        <a
+          href="https://www.edu.goit.global/uk/homepage"
+          target="blank"
+          className={css.link}
+        >
+          <p className={css.linkText}>Пройти курс</p>
+          <BsBoxArrowUpRight className={css.linkIcon} />
+        </a>
+      </div>
+    </div>
+  );
+}
