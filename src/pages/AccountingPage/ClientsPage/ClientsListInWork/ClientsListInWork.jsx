5eda6f30e53e06ee7730f2d678fb81944cad5686
Serhii Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-12-06 16:27:05 +0200

    creating routes and Components for new pages and sub-pages

---
+++ src/pages/AccountingPage/ClientsPage/ClientsListInWork/ClientsListInWork.jsx
@@ -0,0 +1,5 @@
+import css from "./ClientsListInWork.module.css";
+
+export default function ClientsListInWork() {
+    return <div>ClientsListInWork</div>;
+};
---
+++ src/pages/ConnectionsPage/ConnectionsPage.jsx
@@ -0,0 +1,5 @@
+import css from './ConnectionsPage.module.css'
+
+export default function ConnectionsPage() {
+    return <div>ConnectionsPage</div>;
+};
