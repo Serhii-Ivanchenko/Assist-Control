2a2276a897f5551641f70bb865c3f8e44cfbb023
Juliiaks <juliakharetonchuk@gmail.com>
2024-12-06 23:40:04 +0200

    Update AddStaffMemberModal.jsx

--- src/components/Modals/AddStaffMemberModal/AddStaffMemberModal.jsx
+++ src/components/Modals/AddStaffMemberModal/AddStaffMemberModal.jsx
@@ -587,7 +587,7 @@ export default function AddStaffMemberModal({ onClose }) {
                   Графік роботи
                 </label>
                 <AnimatedContent>
-                  <ScheduleTable />
+                  <ScheduleTable isEditing={true} />
                 </AnimatedContent>
               </div>
             </div>
