072a7dd643d2965483b3847240c06b3774e03fe0
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-12-08 14:03:14 +0200

    add changes to input

--- src/components/Modals/AddAutoServiceModal/AddAutoServiceModal.jsx
+++ src/components/Modals/AddAutoServiceModal/AddAutoServiceModal.jsx
@@ -4,7 +4,7 @@ import { AddServiceSchema } from "../../../validationSchemas/addServiceSchema";
 import { FaCheck } from "react-icons/fa";
 import { BsXLg } from "react-icons/bs";
 import { BsThreeDotsVertical } from "react-icons/bs";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import UploadComponent from "../../sharedComponents/UploadComponent/UploadComponent";
 import PopupMenu from "../../sharedComponents/PopupMenu/PopupMenu";
 
@@ -13,6 +13,7 @@ export default function AddAutoServiceModal({ onClose }) {
   const [logo, setLogo] = useState(null);
   const [isInputVisible, setIsInputVisible] = useState(false);
   const [serviceName, setServiceName] = useState("СТО назва");
+  const inputRef = useRef(null);
 
   const initialValues = {
     autoServiceName: "",
@@ -35,6 +36,9 @@ export default function AddAutoServiceModal({ onClose }) {
 
   const onEdit = () => {
     setIsInputVisible(true);
+    setTimeout(() => {
+      inputRef.current?.focus(); // Фокусуємо інпут після оновлення стану
+    }, 0);
   };
 
   const handleBlur = (e) => {
@@ -66,6 +70,9 @@ export default function AddAutoServiceModal({ onClose }) {
             <div className={css.serviceNameWrapper}>
               {isInputVisible ? (
                 <Field
+                  innerRef={inputRef}
+                  value={serviceName}
+                  onChange={(e) => setServiceName(e.target.value)}
                   type="text"
                   name="autoServiceName"
                   className={css.changedInput}
