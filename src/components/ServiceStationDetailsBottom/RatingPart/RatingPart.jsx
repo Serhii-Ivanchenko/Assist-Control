ad137ab3078a23313442f7f2109b3c21562a6224
Serhii Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-12-09 11:12:15 +0200

    fix some styles

--- src/components/ServiceStationDetailsBottom/RatingPart/RatingPart.jsx
+++ src/components/ServiceStationDetailsBottom/RatingPart/RatingPart.jsx
@@ -1,16 +1,16 @@
-import React,  { useState } from "react";
+import React, { useState } from "react";
 import { BsPower, BsFillCaretDownFill } from "react-icons/bs";
 import { RiSave3Fill } from "react-icons/ri";
-import css from './RatingPart.module.css'
+import css from "./RatingPart.module.css";
 
 const action = [
-{ id: 1, name: "Немає події"},
-{ id: 2, name: "1 день після оплати за ремонт" },
-{ id: 3, name: "2 дні після оплати за ремонт" },
-{ id: 4, name: "5 днів після оплати за ремонт" },
-{ id: 5, name: "відразу після оплати за ремонт" },
-{ id: 6, name: "відразу після ЗАВЕРШЕНО" },
-{ id: 7, name: "2 дні після отримання товару" },
+  { id: 1, name: "Немає події" },
+  { id: 2, name: "1 день після оплати за ремонт" },
+  { id: 3, name: "2 дні після оплати за ремонт" },
+  { id: 4, name: "5 днів після оплати за ремонт" },
+  { id: 5, name: "відразу після оплати за ремонт" },
+  { id: 6, name: "відразу після ЗАВЕРШЕНО" },
+  { id: 7, name: "2 дні після отримання товару" },
 ];
 
 const acc = [
@@ -21,24 +21,88 @@ const acc = [
   { id: 5, name: "Формула" },
 ];
 
-const period = [{ id: 1, name: "Кожного разу" },
-{ id: 2, name: "Раз на тиждень" },
+const period = [
+  { id: 1, name: "Кожного разу" },
+  { id: 2, name: "Раз на тиждень" },
 ];
 
 const dictionaries = { action, acc, period };
 
 const data = [
-  { recId: 1, parent: 0, name: "Автосервіс", action: 4, acc: 2, period: 2, activity: true },
+  {
+    recId: 1,
+    parent: 0,
+    name: "Автосервіс",
+    action: 4,
+    acc: 2,
+    period: 2,
+    activity: true,
+  },
   { recId: 2, parent: 1, name: "Співробітників", activity: true },
-  { recId: 3, parent: 2, name: "Керівник", action: 4, acc: 2, period: 1, activity: true },
-  { recId: 4, parent: 2, name: "Менеджер", action: 4, acc: 2, period: 1, activity: true },
-  { recId: 5, parent: 2, name: "Адміністратор", action: 4, acc: 2, period: 1, activity: true },
-  { recId: 6, parent: 2, name: "Механік", action: 4, acc: 2, period: 1, activity: true },
-  { recId: 7, parent: 0, name: "Кліента", action: 4, acc: 2, period: 1, activity: true },
-  { recId: 8, parent: 0, name: "Постачальникі", action: 4, acc: 4, period: 1, activity: true },
-  { recId: 9, parent: 0, name: "Запчастин", action: 4, acc: 4, period: 1, activity: true },
+  {
+    recId: 3,
+    parent: 2,
+    name: "Керівник",
+    action: 4,
+    acc: 2,
+    period: 1,
+    activity: true,
+  },
+  {
+    recId: 4,
+    parent: 2,
+    name: "Менеджер",
+    action: 4,
+    acc: 2,
+    period: 1,
+    activity: true,
+  },
+  {
+    recId: 5,
+    parent: 2,
+    name: "Адміністратор",
+    action: 4,
+    acc: 2,
+    period: 1,
+    activity: true,
+  },
+  {
+    recId: 6,
+    parent: 2,
+    name: "Механік",
+    action: 4,
+    acc: 2,
+    period: 1,
+    activity: true,
+  },
+  {
+    recId: 7,
+    parent: 0,
+    name: "Кліента",
+    action: 4,
+    acc: 2,
+    period: 1,
+    activity: true,
+  },
+  {
+    recId: 8,
+    parent: 0,
+    name: "Постачальникі",
+    action: 4,
+    acc: 4,
+    period: 1,
+    activity: true,
+  },
+  {
+    recId: 9,
+    parent: 0,
+    name: "Запчастин",
+    action: 4,
+    acc: 4,
+    period: 1,
+    activity: true,
+  },
 ];
-
 
 const RatingPart = () => {
   const [openAccordions, setOpenAccordions] = useState({}); // Состояние для аккордеонов
@@ -61,7 +125,9 @@ const RatingPart = () => {
 
   const handleSelectChange = (recId, field, value) => {
     setEditedData((prev) =>
-      prev.map((row) => (row.recId === recId ? { ...row, [field]: value } : row))
+      prev.map((row) =>
+        row.recId === recId ? { ...row, [field]: value } : row
+      )
     );
   };
 
@@ -78,87 +144,112 @@ const RatingPart = () => {
   //   </select>
   // );
 
- 
   const renderSelect = (recId, field, value, dictionary) => (
-  <div className={css.customSelectWrapper}>
-    <select
-      className={`${css.customSelect}`}
-      value={value}
-      onChange={(e) => handleSelectChange(recId, field, e.target.value)}
-    >
-      {dictionary.map((item) => (
-        <option key={item.id} value={item.id} className={css.dictionoption}>
-          {item.name}
-        </option>
-      ))}
-    </select>
-    <BsFillCaretDownFill className={css.customSelectIcon} />
-  </div>
-);
+    <div className={css.customSelectWrapper}>
+      <select
+        className={`${css.customSelect}`}
+        value={value}
+        onChange={(e) => handleSelectChange(recId, field, e.target.value)}
+      >
+        {dictionary.map((item) => (
+          <option key={item.id} value={item.id} className={css.dictionoption}>
+            {item.name}
+          </option>
+        ))}
+      </select>
+      <BsFillCaretDownFill className={css.customSelectIcon} />
+    </div>
+  );
 
   return (
-   <div className={css.container}>
-    <div className={css.divForScroll}>
-      {/* Заголовок */}
-      <div
-        className={css.title}
-      >
-        <div style={{ flex: "0 0 20%" }}></div>
-        <div className={css.titleitem}  style={{ flex: "0 0 20%" }}>Відповідальний</div>
-        <div className={css.titleitem} style={{ flex: "0 0 25%" }}>Подія</div>
-        <div className={css.titleitem} style={{ flex: "0 0 20%" }}>Частота</div>
-        <div style={{ flex: "0 0 3%" }}></div>
-      </div>
+    <div className={css.container}>
+      <div className={css.divForScroll}>
+        {/* Заголовок */}
+        <div className={css.title}>
+          <div style={{ flex: "0 0 20%" }}></div>
+          <div className={css.titleitem} style={{ flex: "0 0 20%" }}>
+            Відповідальний
+          </div>
+          <div className={css.titleitem} style={{ flex: "0 0 25%" }}>
+            Подія
+          </div>
+          <div className={css.titleitem} style={{ flex: "0 0 20%" }}>
+            Частота
+          </div>
+          <div style={{ flex: "0 0 3%" }}></div>
+        </div>
 
-      {/* Рендер строк */}
-      {editedData.map((item) => {
-        const isChild = item.parent === 2;
-        const isAccordion = item.parent === 1;
+        {/* Рендер строк */}
+        {editedData.map((item) => {
+          const isChild = item.parent === 2;
+          const isAccordion = item.parent === 1;
 
-        return (
-          <React.Fragment key={item.recId}>
-            {/* Основные строки parent = 0 */}
-            {item.parent === 0 && (
-              <div className={css.flexitem}
-                
-              >
-                <div style={{ flex: "0 0 20%" }}>{item.name}</div>
-                <div style={{ flex: "0 0 20%" }}>
-                  {renderSelect(item.recId, "acc", item.acc, dictionaries.acc)}
+          return (
+            <React.Fragment key={item.recId}>
+              {/* Основные строки parent = 0 */}
+              {item.parent === 0 && (
+                <div className={css.flexitem}>
+                  <div style={{ flex: "0 0 20%" }}>{item.name}</div>
+                  <div style={{ flex: "0 0 20%" }}>
+                    {renderSelect(
+                      item.recId,
+                      "acc",
+                      item.acc,
+                      dictionaries.acc
+                    )}
+                  </div>
+                  <div style={{ flex: "0 0 25%" }}>
+                    {renderSelect(
+                      item.recId,
+                      "action",
+                      item.action,
+                      dictionaries.action
+                    )}
+                  </div>
+                  <div style={{ flex: "0 0 20%" }}>
+                    {renderSelect(
+                      item.recId,
+                      "period",
+                      item.period,
+                      dictionaries.period
+                    )}
+                  </div>
+                  <div style={{ flex: "0 0 3%" }}>
+                    <BsPower
+                      onClick={(e) => {
+                        e.stopPropagation(); // Предотвращаем закрытие аккордеона
+                        toggleActivity(item.recId);
+                      }}
+                      className={css.iconbtn}
+                      style={{
+                        fill: item.activity
+                          ? "var(--green-btn-normal)"
+                          : "var(--red)",
+                      }}
+                    />
+                  </div>
                 </div>
-                <div style={{ flex: "0 0 25%" }}>
-                   {renderSelect(item.recId, "action", item.action, dictionaries.action)}
-                </div>
-                <div style={{ flex: "0 0 20%" }}>
-                  {renderSelect(item.recId, "period", item.period, dictionaries.period)}
-                </div>
-                <div style={{ flex: "0 0 3%" }}>
-                   <BsPower  onClick={(e) => {
-                      e.stopPropagation(); // Предотвращаем закрытие аккордеона
-                      toggleActivity(item.recId);
-                    }}
-                  className={css.iconbtn}
-                  style={{  fill: item.activity ? "var(--green-btn-normal)" : "var(--red)", }}
-                   /> 
-                </div>
-              </div>
-            )}
+              )}
 
-            {/* Аккордеоны parent = 1 */}
-            {isAccordion && (
-              <div
-                className={css.flexitem} style={{cursor: "pointer"}}
-                onClick={() => toggleAccordion(item.recId)}
-              >
-                <div style={{ flex: "0 0 20%" }} className={css.dictionitemflex}>{item.name}
+              {/* Аккордеоны parent = 1 */}
+              {isAccordion && (
+                <div
+                  className={css.flexitem}
+                  style={{ cursor: "pointer" }}
+                  onClick={() => toggleAccordion(item.recId)}
+                >
+                  <div
+                    style={{ flex: "0 0 20%" }}
+                    className={css.dictionitemflex}
+                  >
+                    {item.name}
                     <BsFillCaretDownFill
-                    className={`${css.iconbtn} ${
-                      openAccordions[item.recId] ? css.rotated : ""
-                      }`
-                   } style={{ marginLeft: "8px"}}
-                  />
-                </div> 
-                {/* <div style={{ flex: "1 0 20%" }}>
+                      className={`${css.iconbtn} ${
+                        openAccordions[item.recId] ? css.rotated : ""
+                      }`}
+                    />
+                  </div>
+                  {/* <div style={{ flex: "1 0 20%" }}>
                   {renderSelect(item.recId, "action", item.action, dictionaries.action)}
                 </div>
                 <div style={{ flex: "1 0 20%" }}>
@@ -167,72 +258,94 @@ const RatingPart = () => {
                 <div style={{ flex: "1 0 20%" }}>
                   {renderSelect(item.recId, "period", item.period, dictionaries.period)}
                 </div> */}
-                {/* <div style={{ flex: "0 0 3%" }}>
+                  {/* <div style={{ flex: "0 0 3%" }}>
                   {openAccordions[item.recId] ? "▲" : "▼"}
                 </div > */}
-                <div  style={{ flex: "0 0 3%" }}>
-                 <BsPower  onClick={(e) => {
-                      e.stopPropagation(); // Предотвращаем закрытие аккордеона
-                      toggleActivity(item.recId);
-                    }}
-                  className={css.iconbtn}
-                  style={{  fill: item.activity ? "var(--green-btn-normal)" : " var(--red)", }}
-                /> 
+                  <div style={{ flex: "0 0 3%" }}>
+                    <BsPower
+                      onClick={(e) => {
+                        e.stopPropagation(); // Предотвращаем закрытие аккордеона
+                        toggleActivity(item.recId);
+                      }}
+                      className={css.iconbtn}
+                      style={{
+                        fill: item.activity
+                          ? "var(--green-btn-normal)"
+                          : " var(--red)",
+                      }}
+                    />
+                  </div>
                 </div>
-              </div>
-            )}
+              )}
 
-            {/* Вложенные строки parent = 2 */}
-            {isChild && openAccordions[item.parent] && (
-              <div
-                className={css.flexitem}
-              >
-                <div style={{ flex: "0 0 20%" , marginLeft: "20px"}} >{item.name}</div>
-                <div style={{ flex: "0 0 20%" , marginLeft: "-20px"}}>
-                  {renderSelect(item.recId, "acc", item.acc, dictionaries.acc)}
+              {/* Вложенные строки parent = 2 */}
+              {isChild && openAccordions[item.parent] && (
+                <div className={css.flexitem}>
+                  <div style={{ flex: "0 0 20%", marginLeft: "20px" }}>
+                    {item.name}
+                  </div>
+                  <div style={{ flex: "0 0 20%", marginLeft: "-20px" }}>
+                    {renderSelect(
+                      item.recId,
+                      "acc",
+                      item.acc,
+                      dictionaries.acc
+                    )}
+                  </div>
+                  <div style={{ flex: "0 0 25%" }}>
+                    {renderSelect(
+                      item.recId,
+                      "action",
+                      item.action,
+                      dictionaries.action
+                    )}
+                  </div>
+                  <div style={{ flex: "0 0 20%" }}>
+                    {renderSelect(
+                      item.recId,
+                      "period",
+                      item.period,
+                      dictionaries.period
+                    )}
+                  </div>
+                  <div style={{ flex: "0 0 3%" }}>
+                    <BsPower
+                      onClick={(e) => {
+                        e.stopPropagation(); // Предотвращаем закрытие аккордеона
+                        toggleActivity(item.recId);
+                      }}
+                      className={css.iconbtn}
+                      style={{
+                        fill: item.activity
+                          ? "var(--green-btn-normal)"
+                          : " var(--red)",
+                      }}
+                    />
+                  </div>
                 </div>
-                <div style={{ flex: "0 0 25%" }}>
-                  
-                  {renderSelect(item.recId, "action", item.action, dictionaries.action)}
-                </div>
-                <div style={{ flex: "0 0 20%" }}>
-                  {renderSelect(item.recId, "period", item.period, dictionaries.period)}
-                </div>
-                <div style={{ flex: "0 0 3%" }}>
-                 
-                  <BsPower  onClick={(e) => {
-                      e.stopPropagation(); // Предотвращаем закрытие аккордеона
-                      toggleActivity(item.recId);
-                    }}
-                  className={css.iconbtn}
-                  style={{  fill: item.activity ? "var(--green-btn-normal)" : " var(--red)", }}
-                   /> 
-                 
-                </div>
-              </div>
-            )}
-          </React.Fragment>
-        );
-      })}
+              )}
+            </React.Fragment>
+          );
+        })}
       </div>
       <div className={css.blockbtn}>
         <button
-        className={css.btncancel}
-        type="button"
-          onClick={() =>  setEditedData(data) }
-      >
-        Відмінити
-      </button>
-      <button
-        className={css.btnsave}
-        type="button"
-        // onClick={() => }
-      >
-        <RiSave3Fill className={css.iconsave} />
-        Зберегти
-      </button>
+          className={css.btncancel}
+          type="button"
+          onClick={() => setEditedData(data)}
+        >
+          Відмінити
+        </button>
+        <button
+          className={css.btnsave}
+          type="button"
+          // onClick={() => }
+        >
+          <RiSave3Fill className={css.iconsave} />
+          Зберегти
+        </button>
       </div>
-   </div> 
+    </div>
   );
 };
 
