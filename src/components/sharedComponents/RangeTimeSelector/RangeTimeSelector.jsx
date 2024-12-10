b8967f8a33d0492981817e9541443f2aa37a277c
Valentine Ukho <petrova.hfhfhf@gmail.com>
2024-12-10 23:51:50 +0200

    crm calendar

--- src/components/sharedComponents/RangeTimeSelector/RangeTimeSelector.jsx
+++ src/components/sharedComponents/RangeTimeSelector/RangeTimeSelector.jsx
@@ -2,10 +2,7 @@ import { useState } from "react";
 import clsx from "clsx";
 import styles from "./RangeTimeSelector.module.css";
 
-export default function RangeTimeSelector({
-  timeData = [],
-  onSelectTimeRange,
-}) {
+export default function RangeTimeSelector({onSelectTimeRange}) {
   const [selected, setSelected] = useState(0);
 
   const options = ["За день", "За тиждень", "За місяць", "Весь час"];
@@ -28,12 +25,6 @@ export default function RangeTimeSelector({
           <p className={styles.timeText}>{option}</p>
         </div>
       ))}
-
-      {timeData.length > 0 && timeData[selected] && (
-        <div className={styles.timeData}>
-          <p>{timeData[selected]}</p>
-        </div>
-      )}
     </div>
   );
 }
