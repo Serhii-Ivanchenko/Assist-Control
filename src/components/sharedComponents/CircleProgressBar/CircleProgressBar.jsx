96c31b1ae0ac3028f648b01772ad0884b1d6f935
Serhii Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-12-11 15:56:17 +0200

    fix space-around in CircularPB

--- src/components/sharedComponents/CircleProgressBar/CircleProgressBar.jsx
+++ src/components/sharedComponents/CircleProgressBar/CircleProgressBar.jsx
@@ -1,24 +1,25 @@
 import css from "./CircleProgressBar.module.css";
-import CircularProgress from '@mui/material/CircularProgress';
+import CircularProgress from "@mui/material/CircularProgress";
 
-export default function CircleProgressBar({value, maxvalue, activecolor, noactivecolor, label, title}) {
-    const percent = maxvalue > 0 ? (value / maxvalue) * 100 : 1;
-    const percentage = maxvalue > 0 ? ((value / maxvalue) * 100 ).toFixed(1): 0;
-   
-    return <div className={css.wrapper}> 
-    
-         <p className={css.title}> {title}  </p>
-        
-        <p className={css.titlenum}> {value}  </p> 
+export default function CircleProgressBar({
+  value,
+  maxvalue,
+  activecolor,
+  noactivecolor,
+  label,
+  title,
+}) {
+  const percent = maxvalue > 0 ? (value / maxvalue) * 100 : 1;
+  const percentage = maxvalue > 0 ? ((value / maxvalue) * 100).toFixed(1) : 0;
 
-        <div style={{
-        position: "relative",
-        display: "inline-block",
-        width: "126px",
-        height: "126px",
-      }}>
+  return (
+    <div className={css.wrapper}>
+      <p className={css.title}> {title} </p>
 
-{/* Неактивный бордер */}
+      <p className={css.titlenum}> {value} </p>
+
+      <div className={css.progressBar}>
+        {/* Неактивный бордер */}
         <CircularProgress
           variant="determinate"
           value={100} // Полный круг
@@ -29,28 +30,27 @@ export default function CircleProgressBar({value, maxvalue, activecolor, noactiv
             position: "absolute",
           }}
         />
-            
-        <CircularProgress variant="determinate" 
-             value={percent} 
-                size="126px"
-                thickness={3}
-      sx={{
-          position: "absolute",
+
+        <CircularProgress
+          variant="determinate"
+          value={percent}
+          size="126px"
+          thickness={3}
+          sx={{
+            position: "absolute",
             top: 0,
             left: 0,
-          color: activecolor, // Цвет активной части
-          '& .MuiCircularProgress-circle': {
-            strokeLinecap: "round", // Закругляем концы линии
-          },
-        }}
-      />
+            color: activecolor, // Цвет активной части
+            "& .MuiCircularProgress-circle": {
+              strokeLinecap: "round", // Закругляем концы линии
+            },
+          }}
+        />
 
-            <div className={css.label}
-          style={{ color: activecolor, }}
-        >
+        <div className={css.label} style={{ color: activecolor }}>
           {label !== undefined ? label : `${percentage}%`}
         </div>
-
+      </div>
     </div>
-    </div>;
-};
+  );
+}
