85f7eabfb36d90efef4c49e627eb21c1298a1e43
Anastasiiya Onyshchuk <shapoval.anastasiiya@gmail.com>
2024-12-16 15:01:11 +0200

    fix styles

--- src/components/ClientsInWorkListSection/ClientsInWorkListSection.jsx
+++ src/components/ClientsInWorkListSection/ClientsInWorkListSection.jsx
@@ -1,8 +1,32 @@
 import ClientsInWorkItem from "./ClientsInWorkItem/ClientsInWorkItem";
-import defImg from "../../assets/images/avatar_default.png";
+import defImg from "../../assets/images/absentAutoImg.webp";
 import css from "./ClientsInWorkListSection.module.css";
 
 const data = [
+  {
+    id: 1,
+    status: "repair",
+    car_model: "HONDA CIVIC",
+    car_img: defImg,
+  },
+  {
+    id: 2,
+    status: "new",
+    car_model: "HONDA CIVIC",
+    car_img: defImg,
+  },
+  {
+    id: 3,
+    status: "complete",
+    car_model: "HONDA CIVIC",
+    car_img: defImg,
+  },
+  {
+    id: 4,
+    status: "complete",
+    car_model: "HONDA CIVIC",
+    car_img: defImg,
+  },
   {
     id: 1,
     status: "repair",
@@ -32,7 +56,7 @@ const data = [
 export default function ClientsInWorkListSection() {
   return (
     <div className={css.wrapper}>
-      <ul>
+      <ul className={css.list}>
         {data.map((item) => (
           <li key={item.id}>
             <ClientsInWorkItem item={item} />
