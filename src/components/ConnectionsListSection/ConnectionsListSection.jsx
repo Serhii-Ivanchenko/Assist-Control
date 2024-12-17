73b81c3eb15c2fdfb6e066aa42374dbc3195ee9a
Serhii Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-12-17 16:22:10 +0200

    fixes

--- src/components/ConnectionsListSection/ConnectionsListSection.jsx
+++ src/components/ConnectionsListSection/ConnectionsListSection.jsx
@@ -7,19 +7,22 @@ import renderStatusCommunication from "../../utils/renderStatusCommunication ";
 import PlayerAndTranscription from "../sharedComponents/PlayerAndTranscription/PlayerAndTranscription";
 import renderStatusCars from "../../utils/renderStatusCars";
 
-
-
 export default function ConnectionsListSection() {
   function renderStatus(itemStatus, css) {
-    const isCommunicationStatus = ["all_appeal", "new_appeal", "client", "missing", "appointment"].includes(itemStatus);
-    
+    const isCommunicationStatus = [
+      "all_appeal",
+      "new_appeal",
+      "client",
+      "missing",
+      "appointment",
+    ].includes(itemStatus);
+
     if (isCommunicationStatus) {
       return renderStatusCommunication(itemStatus, false, css, false);
     }
-  
+
     return renderStatusCars(itemStatus, false, css, false);
   }
-
 
   return (
     <div className={css.wrapper}>
@@ -33,30 +36,28 @@ export default function ConnectionsListSection() {
           </div>
           <div className={css.rightContainer}>
             <div className={css.userContainer}>
-            <div className={css.avatar}>
-              <img
-                src={item.photoUrl || defaultAvatar} 
-                alt={item.name}
-                className={css.avatarImage}
-              />
-            </div>
-            <div className={css.name}>{item.name}</div>
+              <div className={css.avatar}>
+                <img
+                  src={item.photoUrl || defaultAvatar}
+                  alt={item.name}
+                  className={css.avatarImage}
+                />
+              </div>
+              <div className={css.name}>{item.name}</div>
             </div>
             <div className={css.auto}>{item.auto}</div>
-            <div className={css.status}>{renderStatus(item.status, css)}
-            </div>
+            <div className={css.status}>{renderStatus(item.status, css)}</div>
             <div className={css.audioContainer}>
               <PlayerAndTranscription
-          sizePlayer="small"
-          sizeBtn="small"
-          summary={summary}
-          messages={messages}
-          // audio={audio}
-          showPhoto={false}
-          accounting={true}
-        />
+                sizePlayer="small"
+                sizeBtn="small"
+                summary={summary}
+                messages={messages}
+                // audio={audio}
+                showPhoto={false}
+                accounting={true}
+              />
             </div>
-            
           </div>
         </div>
       ))}
