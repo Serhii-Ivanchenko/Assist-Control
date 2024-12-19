8d534c0d108b92a92b31e287276688363e1e0833
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-12-19 22:54:24 +0200

    create all necessary 4 redux till warehouse (acc.to my list)

--- src/redux/settings/operations.js
+++ src/redux/settings/operations.js
@@ -50,23 +50,26 @@ export const updateWorkSchedule = createAsyncThunk(
 );
 
 // Get list of service posts
-export const getPosts = createAsyncThunk("settings/getPosts", async (_, thunkAPI) => {
-  const state = thunkAPI.getState();
-  const serviceId = state.auth.userData.selectedServiceId;
-  try {
-    const response = await axiosInstance.get(`/set/get_posts/`, {
-      headers: {
-        "X-Api-Key": "YA7NxysJ",
-        "company-id": serviceId,
-      },
-    });
-    console.log("getPosts", response.data);
+export const getPosts = createAsyncThunk(
+  "settings/getPosts",
+  async (_, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const serviceId = state.auth.userData.selectedServiceId;
+    try {
+      const response = await axiosInstance.get(`/set/get_posts/`, {
+        headers: {
+          "X-Api-Key": "YA7NxysJ",
+          "company-id": serviceId,
+        },
+      });
+      console.log("getPosts", response.data);
 
-    return response.data;
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error.message);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
   }
-});
+);
 
 // Create new post
 export const createPost = createAsyncThunk(
@@ -96,8 +99,8 @@ export const updatePostStatus = createAsyncThunk(
   async (newStatus, thunkAPI) => {
     const state = thunkAPI.getState();
     const serviceId = state.auth.userData.selectedServiceId;
-      try {
-         const { postId, ...status } = newStatus;
+    try {
+      const { postId, ...status } = newStatus;
       const response = await axiosInstance.patch(
         `/set/update_post_status/${postId}`,
         status,
@@ -147,12 +150,34 @@ export const updatePostData = createAsyncThunk(
 // Delete post
 export const deletePost = createAsyncThunk(
   "settings/deletePost",
-    async (postId, thunkAPI) => {
-       const state = thunkAPI.getState();
-       const serviceId = state.auth.userData.selectedServiceId;
+  async (postId, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const serviceId = state.auth.userData.selectedServiceId;
     try {
-      const response = await axiosInstance.patch(
-        `/set/update_post/${postId}`,
+      const response = await axiosInstance.patch(`/set/update_post/${postId}`, {
+        headers: {
+          "X-Api-Key": "YA7NxysJ",
+          "company-id": serviceId,
+        },
+      });
+      console.log("deletePost", response.data);
+
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+// Get categories and services for PricePart
+export const getPrices = createAsyncThunk(
+  "settings/getPrices",
+  async (_, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const serviceId = state.auth.userData.selectedServiceId;
+    try {
+      const response = await axiosInstance.get(
+        `/v1/categories_services/prices`,
         {
           headers: {
             "X-Api-Key": "YA7NxysJ",
@@ -160,7 +185,137 @@ export const deletePost = createAsyncThunk(
           },
         }
       );
-      console.log("deletePost", response.data);
+      console.log("getPrices", response.data);
+
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+// Edit prices
+export const editPrices = createAsyncThunk(
+  "settings/editPrices",
+  async (newPrices, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const serviceId = state.auth.userData.selectedServiceId;
+    try {
+      const { service_id, ...prices } = newPrices;
+      const response = await axiosInstance.patch(
+        `/v1/services/${service_id}/prices`,
+        prices,
+        {
+          headers: {
+            "X-Api-Key": "YA7NxysJ",
+            "company-id": serviceId,
+          },
+        }
+      );
+      console.log("editPrices", response.data);
+
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+// Create category of services
+export const createCategory = createAsyncThunk(
+  "settings/createCategory",
+  async (newCategory, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const serviceId = state.auth.userData.selectedServiceId;
+    try {
+      const response = await axiosInstance.post(
+        `/v1/categories_services/`,
+        newCategory,
+        {
+          headers: {
+            "X-Api-Key": "YA7NxysJ",
+            "company-id": serviceId,
+          },
+        }
+      );
+      console.log("createCategory", response.data);
+
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+// Update category name
+export const updateCategoryData = createAsyncThunk(
+  "settings/updateCategoryData",
+  async (categoryDataToUpdate, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const serviceId = state.auth.userData.selectedServiceId;
+    try {
+      const { categoryId, ...dataToUpdate } = categoryDataToUpdate;
+      const response = await axiosInstance.patch(
+        `/v1/categories_services/${categoryId}`,
+        dataToUpdate,
+        {
+          headers: {
+            "X-Api-Key": "YA7NxysJ",
+            "company-id": serviceId,
+          },
+        }
+      );
+      console.log("updateCategoryData", response.data);
+
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+// Create service
+export const createService = createAsyncThunk(
+  "settings/createService",
+  async (newService, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const serviceId = state.auth.userData.selectedServiceId;
+
+    const { categoryId, ...serviceName } = newService;
+
+    try {
+      const response = await axiosInstance.post(`/v1/services/`, serviceName, {
+        headers: {
+          "X-Api-Key": "YA7NxysJ",
+          "company-id": serviceId,
+        },
+      });
+      console.log("createService", response.data);
+
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+// Delete service
+export const deleteService = createAsyncThunk(
+  "settings/deleteService",
+  async (service_id, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const serviceId = state.auth.userData.selectedServiceId;
+    try {
+      const response = await axiosInstance.patch(
+        `/v1/services/${service_id}`,
+        {
+          headers: {
+            "X-Api-Key": "YA7NxysJ",
+            "company-id": serviceId,
+          },
+        }
+      );
+      console.log("deleteService", response.data);
 
       return response.data;
     } catch (error) {
