6f6b3f26c729b88eeeccc80e1037748984f1da7b
Juliiaks <juliakharetonchuk@gmail.com>
2024-12-28 15:42:52 +0200

    added select

--- src/components/Chat/LeftSection/MessagesPart/SearchByMessages/SearchByMessages.jsx
+++ src/components/Chat/LeftSection/MessagesPart/SearchByMessages/SearchByMessages.jsx
@@ -2,24 +2,44 @@ import { useState } from "react";
 import css from "./SearchByMessages.module.css";
 import { IoIosSearch } from "react-icons/io";
 import { IoIosArrowDown } from "react-icons/io";
+import { BiMessageRoundedDetail } from "react-icons/bi";
+import { HiOutlineHashtag } from "react-icons/hi";
 
 export default function SearchByMessages() {
   const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null);
 
   const handleOpen = () => {
     setIsOpen((prev) => !prev);
   };
 
+  const handleOptionClick = (option) => {
+    setSelectedOption(option);
+    setIsOpen(false);
+  };
+
   const options = [
-    { value: "message", text: "Пошук по повідомленням" },
-    { value: "teg", text: "Пошук по тегам" },
+    {
+      value: "message",
+      text: "Пошук по повідомленням",
+      icon: <BiMessageRoundedDetail />,
+    },
+    { value: "teg", text: "Пошук по тегам", icon: <HiOutlineHashtag /> },
   ];
 
   return (
     <div>
       <div className={css.inputBox}>
         <IoIosSearch className={css.iconSearch} size={18} />
-        <input className={css.input} placeholder="Пошук по повідомленням" />
+        <input
+          className={css.input}
+          placeholder={
+            selectedOption
+              ? options.find((option) => option.value === selectedOption.value)
+                  ?.text
+              : "Пошук по повідомленням"
+          }
+        />
         <IoIosArrowDown
           className={css.iconArrow}
           size={20}
@@ -27,14 +47,14 @@ export default function SearchByMessages() {
         />
       </div>
       {isOpen && (
-        <div>
+        <div className={css.optionsBox}>
           {options.map((option) => (
             <div
               key={option.value}
               className={css.option}
-              // onClick={() => handleOptionClick(lang)}
+              onClick={() => handleOptionClick(option)}
             >
-              <span className={css.icon} />
+              <span className={css.icon}>{option.icon}</span>
               {option.text}
             </div>
           ))}
