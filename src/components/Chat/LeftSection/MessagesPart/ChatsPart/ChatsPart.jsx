688857d3c76ceb81184f7ac87105681caf266032
Juliiaks <juliakharetonchuk@gmail.com>
2024-12-30 23:26:07 +0200

    style edits

--- src/components/Chat/LeftSection/MessagesPart/ChatsPart/ChatsPart.jsx
+++ src/components/Chat/LeftSection/MessagesPart/ChatsPart/ChatsPart.jsx
@@ -5,6 +5,7 @@ import assist from "../../../../../assets/images/ChannelsImages/logo-rect 1.png"
 import facebook from "../../../../../assets/images/ChannelsImages/Facebook_Messenger_1.png";
 import avatar from "../../../../../assets/images/avatar_default.png";
 import css from "./ChatsPart.module.css";
+import { useState } from "react";
 
 export default function ChatsPart() {
   const chats = [
@@ -16,6 +17,7 @@ export default function ChatsPart() {
         "Вітаю! Чи можна записатися на діагностику електрики та двигу...",
       managersPhoto: avatar,
       time: "4m ago",
+      read: false,
     },
     {
       avatar: avatar,
@@ -25,6 +27,8 @@ export default function ChatsPart() {
         "Доброго дня! У мене є питання щодо ремонту коробки переда...",
       managersPhoto: avatar,
       time: "15m ago",
+      warning: true,
+      read: false,
     },
     {
       avatar: avatar,
@@ -34,6 +38,7 @@ export default function ChatsPart() {
         "Привіт! Чи є вільні місця для запису на наступний тиждень у...",
       managersPhoto: avatar,
       time: "15m ago",
+      read: true,
     },
     {
       avatar: avatar,
@@ -43,6 +48,7 @@ export default function ChatsPart() {
         "Дякую за швидку відповідь! Я хотіла б уточнити вартість зам...",
       managersPhoto: avatar,
       time: "2h ago",
+      read: true,
     },
     {
       avatar: avatar,
@@ -51,6 +57,7 @@ export default function ChatsPart() {
       lastMessage: "Доброго ранку! Ви працюєте з автомобілями американськог...",
       managersPhoto: avatar,
       time: "3h ago",
+      read: true,
     },
     {
       avatar: avatar,
@@ -60,14 +67,28 @@ export default function ChatsPart() {
         "Доброго дня! У мене є питання щодо ремонту коробки переда...",
       managersPhoto: avatar,
       time: "1d ago",
+      read: true,
     },
   ];
+
+  const [chosen, setChosen] = useState(false);
+
+  const handleChoose = (index) => {
+    // setChosen(chosen === index ? null : index); // По повторному кліку вибір знімається
+    setChosen(index);
+  };
 
   return (
     <div>
       <ul className={css.chatsList}>
         {chats.map((chat, index) => (
-          <li key={index} className={css.chatsListItem}>
+          <li
+            key={index}
+            className={`${css.chatsListItem} ${
+              chat.warning && css.warningBorder
+            } ${chosen === index && css.chosenChat}`}
+            onClick={() => handleChoose(index)}
+          >
             <div className={css.avatarBox}>
               <img src={chat.avatar} alt="" className={css.avatar} />
               <img
@@ -81,7 +102,13 @@ export default function ChatsPart() {
             </div>
             <div className={css.nameAndMessageBox}>
               <p className={css.name}>{chat.name}</p>
-              <p className={css.lastMessage}>{chat.lastMessage}</p>
+              <p
+                className={`${css.lastMessage} ${
+                  !chat.read && css.freshMessage
+                }`}
+              >
+                {chat.lastMessage}
+              </p>
             </div>
             <div className={css.timeContainer}>
               <div className={css.managersPhotoBox}>
