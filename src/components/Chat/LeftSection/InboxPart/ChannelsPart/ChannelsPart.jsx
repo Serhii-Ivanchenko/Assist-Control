fb3b16aef8f59e676c77bdcdcb68c23ffbea2530
Juliiaks <juliakharetonchuk@gmail.com>
2025-01-09 23:05:01 +0200

    normal dragging

--- src/components/Chat/LeftSection/InboxPart/ChannelsPart/ChannelsPart.jsx
+++ src/components/Chat/LeftSection/InboxPart/ChannelsPart/ChannelsPart.jsx
@@ -6,19 +6,8 @@ import facebook from "../../../../../assets/images/ChannelsImages/Facebook_Messe
 import whatsApp from "../../../../../assets/images/ChannelsImages/WhatsApp_1.png";
 import assist from "../../../../../assets/images/ChannelsImages/logo-rect 1.png";
 import { BsGlobe } from "react-icons/bs";
-
 import { useState } from "react";
-// import { useRef } from "react";
 import ChannelItem from "./ChannelItem/ChannelItem";
-// import {
-//   DndContext,
-//   // useDraggable,
-//   useDroppable,
-//   // DragEndEvent,
-// } from "@dnd-kit/core";
-// import { CSS } from "@dnd-kit/utilities";
-// import { DragOverlay } from "@dnd-kit/core";
-// import SortableOverlay from "./SortableOverlay/SortableOverlay";
 
 export default function ChannelsPart({
   handleFilter,
@@ -89,68 +78,12 @@ export default function ChannelsPart({
     setIsActive(id);
   };
 
-  //Функціонал бібліотеки з прикольним перетягуванням, яке хочу зробити
-
-  // const [activeItem, setActiveItem] = useState(null);
-  // const [activeItemPosition, setActiveItemPosition] = useState({
-  //   x: 0,
-  //   y: 0,
-  // });
-
-  // const { setNodeRef } = useDroppable({
-  //   id: "droppable",
-  // });
-
-  // const handleDragEnd = (event) => {
-  //   const { active, over } = event;
-
-  //   if (active && over && active.id !== over?.id) {
-  //     console.log("Active ID:", active.id);
-  //     console.log("Over ID:", over?.id);
-
-  //     const oldIndex = channels.findIndex((item) => item.id === active.id);
-  //     console.log(oldIndex);
-
-  //     const newIndex = channels.findIndex((item) => item.id === over.id);
-  //     console.log(newIndex);
-
-  //     if (oldIndex === -1 || newIndex === -1) {
-  //       console.error("Invalid indices:", { oldIndex, newIndex });
-  //       return; // Якщо індекси некоректні, припиняємо виконання
-  //     }
-
-  //     // Reorder the list
-  //     // if (oldIndex !== newIndex) {
-  //     // Створюємо копію списку і змінюємо порядок елементів
-  //     // const newItems = [...channels];
-  //     // const [removed] = newItems.splice(oldIndex, 1); // Видаляємо елемент з поточної позиції
-  //     // newItems.splice(newIndex, 0, removed); // Додаємо на нову позицію
-  //     const newItems = Array.from(channels);
-  //     const [removed] = newItems.splice(oldIndex, 1);
-  //     newItems.splice(newIndex, 0, removed);
-
-  //     setChannels(newItems); // Оновлюємо стан з новим порядком
-  //     // }
-  //   }
-  //   setActiveItem(null);
-  // };
-
-  // const handleDragStart = (event) => {
-  //   setActiveItem(event.active.id); // Set the active item when dragging starts
-  //   const { clientX, clientY } = event; // Get the starting position of the drag
-  //   setActiveItemPosition({ x: clientX, y: clientY }); // Store the initial position
-  // };
-
-  // const handleDragMove = (event) => {
-  //   const { clientX, clientY } = event;
-  //   setActiveItemPosition({ x: clientX, y: clientY }); // Update position during drag
-  // };
-
   //Функціонал, який працює
   const [draggedItemIndex, setDraggedItemIndex] = useState(null);
 
-  const handleDragStart = (index) => {
+  const handleDragStart = (e, index) => {
     setDraggedItemIndex(index);
+    e.dataTransfer.setData("text/plain", index);
   };
 
   const handleDragOver = (event, index) => {
@@ -164,11 +97,6 @@ export default function ChannelsPart({
       setDraggedItemIndex(index);
       setChannels(updatedItems);
     }
-  };
-
-  // Закінчення перетягування
-  const handleDragEnd = () => {
-    setDraggedItemIndex(null);
   };
 
   return (
@@ -192,15 +120,7 @@ export default function ChannelsPart({
       </div>
 
       {isOpen && (
-        // <DndContext
-        //   onDragEnd={handleDragEnd}
-        //   onDragStart={handleDragStart}
-        //   onDragMove={handleDragMove}
-        // >
-        <ul
-          className={css.channelsList}
-          // ref={setNodeRef}
-        >
+        <ul className={css.channelsList}>
           {channels.map((channel, index) => (
             <ChannelItem
               key={channel.id}
@@ -210,32 +130,13 @@ export default function ChannelsPart({
               gmail={gmail}
               facebook={facebook}
               handleFilter={handleFilter}
-              // moveChannel={moveChannel}
-              handleDragEnd={handleDragEnd}
-              handleDragStart={handleDragStart}
               handleDragOver={handleDragOver}
               isActive={isActive}
               handleIsActive={handleIsActive}
+              onDragStart={handleDragStart}
             />
           ))}
         </ul>
-        //<SortableOverlay>
-        // {activeItem ? (
-        //   <ChannelItem
-        //     key={activeItem}
-        //     id={activeItem}
-        //     channel={channels.find((channel) => channel.id === activeItem)}
-        //     style={{
-        //       position: "absolute",
-        //       top: `${activeItemPosition.y}px`, // Position from top
-        //       left: `${activeItemPosition.x}px`, // Position from left
-        //       pointerEvents: "none", // Prevent overlay from blocking the drag
-        //       zIndex: 999, // Make sure it appears on top of other elements
-        //     }}
-        //   />
-        // ) : null}
-        // </SortableOverlay>
-        // </DndContext> */}
       )}
     </div>
   );
