481cae3b4fab0f58a0eb00a64c6725fcdcb4d2b3
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2025-01-17 18:06:23 +0200

    add operations to get chats and chatDetails

--- src/redux/chat/operations.js
+++ src/redux/chat/operations.js
@@ -9,15 +9,106 @@ import { axiosInstance } from "../../services/api.js";
 
 // Get Chats
 
+export const getChats = createAsyncThunk("chat/getChats", async (_, thunkAPI) => {
+  // const state = thunkAPI.getState();
+  // const serviceId = state.auth.userData.selectedServiceId;
+  try {
+    const response = await axiosInstance.get(`/chats`, {
+      headers: {
+        // "X-Api-Key": "YA7NxysJ",
+        //   "company-id": serviceId,
+      },
+    });
+    console.log("getChats", response.data);
+
+    return response.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
+});
+
 
 // Get Sorted Chats
+
+export const getSortedChats = createAsyncThunk(
+  "chat/getSortedChats",
+  async (sortData, thunkAPI) => {
+    // const state = thunkAPI.getState();
+    // const serviceId = state.auth.userData.selectedServiceId;
+    try {
+      const { sortBy, sortOrder } = sortData;
+      // sortBy: date/priority/source
+      // sortOrder: asc or desc
+      const response = await axiosInstance.get(
+        `/chats/sorted?sort_by=${sortBy}&order=${sortOrder}`,
+        {
+          headers: {
+            // "X-Api-Key": "YA7NxysJ",
+            //   "company-id": serviceId,
+          },
+        }
+      );
+      console.log("getSortedChats", response.data);
+
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
 
 
 // Update Chat Status
 
+export const updateChatStatus = createAsyncThunk(
+  "chat/updateChatStatus",
+  async (chatStatus, thunkAPI) => {
+    // const state = thunkAPI.getState();
+    // const serviceId = state.auth.userData.selectedServiceId;
+    try {
+      const { chat_id, status } = chatStatus;
+      //statuses: new, in_progress, closed
+      const response = await axiosInstance.patch(
+        `/chats/${chat_id}/status?status=${status}`,
+        {
+          headers: {
+            // "X-Api-Key": "YA7NxysJ",
+            // "company-id": serviceId,
+          },
+        }
+      );
+      console.log("updateChatStatus", response.data);
+
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
 
 // Get Chat Details
 
+export const getChatDetails = createAsyncThunk(
+  "chat/getChatDetails",
+  async (chat_id, thunkAPI) => {
+    // const state = thunkAPI.getState();
+    // const serviceId = state.auth.userData.selectedServiceId;
+    try {
+      const response = await axiosInstance.get(`/chats/${chat_id}`, {
+        headers: {
+          // "X-Api-Key": "YA7NxysJ",
+          //   "company-id": serviceId,
+        },
+      });
+      console.log("getChatDetails", response.data);
+
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
 
 // Add Comment
 
