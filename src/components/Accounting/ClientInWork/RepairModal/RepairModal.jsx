8dc156f2ad740b4fc7df39fd156bb86bbab8a76e
Valentine Ukho <petrova.hfhfhf@gmail.com>
2025-02-14 20:09:03 +0200

    Create repair modaL

--- src/components/Accounting/ClientInWork/RepairModal/RepairModal.jsx
+++ src/components/Accounting/ClientInWork/RepairModal/RepairModal.jsx
@@ -1,11 +1,21 @@
 import styles from "./RepairModal.module.css";
 import CarInfo from "../../../sharedComponents/CarInfo/CarInfo";
-import { BsPersonLinesFill, BsWrench } from "react-icons/bs";
+import { BsCheckLg, BsPersonLinesFill, BsWrench } from "react-icons/bs";
 import { repairData } from "./repairData";
 import TableRepair from "./TableRepair/TableRepair";
+import DownloadPdfButtonRepair from "../../../sharedComponents/Pdf/DownloadPdfButtonRepair/DownloadPdfButtonRepair";
+import { MdClose } from "react-icons/md";
+import { useState } from "react";
 
 
-const RepairModal = ({ car }) => {
+const RepairModal = ({ car, onClose }) => {
+  const [data, setData] = useState(repairData);
+
+  const handleDelete = (id) => {
+    const updatedData = data.filter((item) => item.id !== id);
+    setData(updatedData);
+  };
+
   const carInfo = {
     clientName: car.client_name,
     clientPhone: car.client_phone,
@@ -18,6 +28,8 @@ const RepairModal = ({ car }) => {
     mileage: car.mileage,
   };
 
+  const price = repairData.reduce((total, item) => total + (item.soldAmount || 0), 0);
+
   return (
     <div className={styles.modalRepair}>
       <div className={styles.infoContainer}>
@@ -25,7 +37,7 @@ const RepairModal = ({ car }) => {
           <CarInfo {...carInfo} />
         </div>
         <div>
-          <p>Ремонт № 345</p>
+          <p className={styles.title}>Ремонт № 345</p>
         </div>
         <div className={styles.workInfo}>
           <div className={styles.date}>03.02.2025</div>
@@ -53,8 +65,30 @@ const RepairModal = ({ car }) => {
         </div>
       </div>
       <div className={styles.tableContainer}>
-      <TableRepair data={repairData} />
+      <TableRepair data={data} onDelete={handleDelete}/>
       </div>
+      <div className={styles.bottomInfoContainer}>
+        <div className={styles.leftContainer}>
+        <div className={styles.repairPrice}>
+        <span className={styles.titleBtn}>Ремонт</span>
+        <div className={styles.priceBox}>{price}</div>
+        </div>
+        <div className={styles.differencePay}>
+        <span className={styles.titleBtn}>Залишок</span>
+        <div className={styles.priceBox}>-18000</div>
+        </div>
+        <button className={styles.btnPayment}> <BsCheckLg size={16} color="white"/>
+        Оплата</button>
+        </div>
+        <div className={styles.rightContainer}>
+        <DownloadPdfButtonRepair carsData={repairData} />
+        <button className={styles.btnCancel}>Скасувати</button>
+          <button className={styles.btnDone}>Готово</button>
+        </div>
+      </div>
+      <button className={styles.closeButton} onClick={onClose}>
+              <MdClose color="var(--input-stroke)" size={22} />
+            </button>
     </div>
   );
 };
